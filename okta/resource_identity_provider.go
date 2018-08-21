86e429405e07f7408e57d486c7556ef13d2774ea
Purrbiscuit <spurr@articulate.com>
2018-08-21 13:46:14 -0400

    update some default values in idp provider schema

--- okta/resource_identity_provider.go
+++ okta/resource_identity_provider.go
@@ -14,7 +14,6 @@ func resourceIdentityProviders() *schema.Resource {
 		Read:          resourceIdentityProviderRead,
 		Update:        resourceIdentityProviderUpdate,
 		Delete:        resourceIdentityProviderDelete,
-		CustomizeDiff: func(d *schema.ResourceDiff, v interface{}) error { return nil },
 
 		Schema: map[string]*schema.Schema{
 			"type": &schema.Schema{
@@ -31,7 +30,7 @@ func resourceIdentityProviders() *schema.Resource {
 			"protocol_type": &schema.Schema{
 				Type:        schema.TypeString,
 				Optional:    true,
-				Default:     "OAUTH2",
+				Default:     "OIDC",
 				Description: "IDP Protocol type to use - ie. OAUTH2",
 			},
 			"protocol_scopes": &schema.Schema{
@@ -51,16 +50,102 @@ func resourceIdentityProviders() *schema.Resource {
 				Description: "OAUTH2 client secret",
 			},
 			"status": &schema.Schema{
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"authorization_url": &schema.Schema{
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"authorization_url_binding": &schema.Schema{
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
+			},
+			"policy_provisioning_action": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "AUTO",
+				Description: "Policy Provisioning Action",
+			},
+			"policy_provisioning_profile_master": &schema.Schema{
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     true,
+				Description: "Policy Provisioning Profile Master",
+			},
+			"policy_provisioning_groups_action": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "NONE",
+				Description: "Policy Provisioning Groups Action",
+			},
+			"policy_provisioning_conditions_deprovisioned_action": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "NONE",
+				Description: "Policy Provisioning Conditions Deprovisioned Action",
+			},
+			"policy_provisioning_conditions_suspended_action": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "NONE",
+				Description: "Policy Provisioning Conditions Suspended Action",
+			},
+			"policy_account_link_filter": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Policy Account Link Filter",
+			},
+			"policy_account_link_action": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "AUTO",
+				Description: "Policy Account Link Action",
+			},
+			"policy_subject_username_template": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "idpuser.firstName",
+				Description: "Policy Subject Username Template",
+			},
+			"policy_subject_filter": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Policy Subject Filter",
+			},
+			"policy_subject_match_type": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "USERNAME",
+				Description: "Policy Subject Match Type",
+			},
+			"policy_max_clock_skew": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     0,
+				Description: "Policy Max Clock Skew",
+			},
+			"links_authorized_href": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"links_authorized_templated": &schema.Schema{
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"links_authorized_hints_allow": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"links_client_redirect_uri_href": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"links_client_redirect_uri_hints_allow": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
 		},
 	}
@@ -76,20 +161,20 @@ func resourceIdentityProviderCreate(d *schema.ResourceData, m interface{}) error
 	credentials := &okta.Credentials{Client: idpClient}
 	protocol := &okta.Protocol{Credentials: credentials}
 
-	idpGroups := &okta.IdpGroups{Action:"NONE"}
-	deprovisioned := &okta.Deprovisioned{Action:"NONE"}
-	suspended := &okta.Suspended{Action:"NONE"}	
-	
+	idpGroups := &okta.IdpGroups{Action: "NONE"}
+	deprovisioned := &okta.Deprovisioned{Action: "NONE"}
+	suspended := &okta.Suspended{Action: "NONE"}
+
 	conditions := &okta.Conditions{
 		Deprovisioned: deprovisioned,
-		Suspended: suspended,
+		Suspended:     suspended,
 	}
 
 	provisioning := &okta.Provisioning{
-		Action: "AUTO",
+		Action:        "AUTO",
 		ProfileMaster: true,
-		Groups: idpGroups,
-		Conditions: conditions,
+		Groups:        idpGroups,
+		Conditions:    conditions,
 	}
 
 	accountLink := &okta.AccountLink{
@@ -99,16 +184,16 @@ func resourceIdentityProviderCreate(d *schema.ResourceData, m interface{}) error
 	userNameTemplate := &okta.UserNameTemplate{
 		Template: "idpuser.firstName",
 	}
-	
+
 	subject := &okta.Subject{
 		UserNameTemplate: userNameTemplate,
-		MatchType: "USERNAME",
+		MatchType:        "USERNAME",
 	}
-	
+
 	policy := &okta.IdpPolicy{
 		Provisioning: provisioning,
-		AccountLink: accountLink,
-		Subject: subject,
+		AccountLink:  accountLink,
+		Subject:      subject,
 		MaxClockSkew: 0,
 	}
 
@@ -125,15 +210,15 @@ func resourceIdentityProviderCreate(d *schema.ResourceData, m interface{}) error
 		protocol.Scopes = scopes
 	}
 
-	protocol.Credentials.Client.ClientID     = d.Get("client_id").(string)
+	protocol.Credentials.Client.ClientID = d.Get("client_id").(string)
 	protocol.Credentials.Client.ClientSecret = d.Get("client_secret").(string)
 
 	idp.Protocol = protocol
-	idp.Policy   = policy
+	idp.Policy = policy
 
 	returnedIdp, _, err := client.IdentityProviders.CreateIdentityProvider(idp)
 
-	d.SetId(returnedIdp.ID);
+	d.SetId(returnedIdp.ID)
 	if err != nil {
 		return err
 	}
@@ -153,7 +238,8 @@ func resourceIdentityProviderRead(d *schema.ResourceData, m interface{}) error {
 	if exists == true {
 		idp, _, err = client.IdentityProviders.GetIdentityProvider(d.Id())
 	} else {
-		return fmt.Errorf("[ERROR] Error Identity Provider not found in Okta: %v", err)
+		d.SetId("")
+		return nil
 	}
 
 	d.Set("type", idp.Type)
@@ -161,7 +247,7 @@ func resourceIdentityProviderRead(d *schema.ResourceData, m interface{}) error {
 	d.Set("status", idp.Status)
 	d.Set("authorization_url", idp.Protocol.Endpoints.Authorization.Url)
 	d.Set("authorization_url_binding", idp.Protocol.Endpoints.Authorization.Binding)
-	d.Set("protocol_type", idp.Protocol.Type)	
+	d.Set("protocol_type", idp.Protocol.Type)
 	d.Set("protocol_scopes", idp.Protocol.Scopes)
 	d.Set("policy_provisioning_action", idp.Policy.Provisioning.Action)
 	d.Set("policy_provisioning_profile_master", idp.Policy.Provisioning.ProfileMaster)
@@ -200,8 +286,15 @@ func resourceIdentityProviderDelete(d *schema.ResourceData, m interface{}) error
 		if err != nil {
 			return fmt.Errorf("[ERROR] Error Deleting Identity Providers from Okta: %v", err)
 		}
-	} else {
-		return fmt.Errorf("[ERROR] Error Identity Provider not found in Okta: %v", err)
+	}
+
+	updatedExists, err2 := idpExists(d, m)
+	if err2 != nil {
+		return err2
+	}
+
+	if updatedExists == true {
+		return fmt.Errorf("%v", "Resource Still Exits after Destroy called")
 	}
 	return nil
 }
@@ -210,6 +303,7 @@ func resourceIdentityProviderDelete(d *schema.ResourceData, m interface{}) error
 func idpExists(d *schema.ResourceData, m interface{}) (bool, error) {
 	client := m.(*Config).oktaClient
 	_, _, err := client.IdentityProviders.GetIdentityProvider(d.Id())
+
 	if client.OktaErrorCode == "E0000007" {
 		return false, nil
 	}
