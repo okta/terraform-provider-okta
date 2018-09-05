c362c8ad2af40398720a8dae86010f5e4e886c51
Purrbiscuit <spurr@articulate.com>
2018-09-05 15:36:43 -0400

    initial okta_trusted_origins resource commit

---
+++ okta/trusted_origin.go
@@ -0,0 +1,112 @@
+package okta
+
+import (
+  "github.com/articulate/oktasdk-go/okta"
+  "github.com/hashicorp/terraform/helper/schema"
+  "github.com/hashicorp/terraform/helper/validation"
+)
+
+func resourceTrustedOrigins() *schema.Resource {
+  return &schema.Resource{
+    Create: resourceTrustedOriginCreate,
+    Read:   resourceTrustedOriginRead,
+    Update: resourceTrustedOriginUpdate,
+    Delete: resourceTrustedOriginDelete,
+    Exists: trustedOriginExists,
+    Importer: &schema.ResourceImporter{
+      State: schema.ImportStatePassthrough,
+    },
+
+    Schema: map[string]*schema.Schema{
+      "active": &schema.Schema{
+        Type:        schema.TypeBool,
+        Optional:    true,
+        Default:     true,
+        Description: "Whether the Trusted Origin is active or not - can only be issued post-creation",
+      },
+      "id": &schema.Schema{
+        Type:        schema.TypeString,
+        Computed:    true,
+        Description: "ID of the Trusted Origin",
+      },
+      "name": &schema.Schema{
+        Type:        schema.TypeString,
+        Required:    true,
+        Description: "Name of the Trusted Origin Resource",
+      },
+      "origin": &schema.Schema{
+        Type:        schema.TypeString,
+        Required:    true,
+        Description: "The origin to trust",
+      },
+      "scopes": &schema.Schema{
+        Type:        schema.TypeList,
+        Optional:    true,
+        Elem:        &schema.Schema{Type: schema.TypeString},
+        ValidateFunc: validation.StringInSlice([]string{"CORS", "Redirect"}, false),
+        Description: "Scopes of the Trusted Origin - can either be CORS or Redirect only",
+      },
+    },
+  }
+}
+
+func assembleTrustedOrigin() *okta.TrustedOrigin {
+  hints := &okta.TrustedOriginHints{}
+
+  deactivate := &okta.TrustedOriginDeactive{
+    Hints: hints,
+  }
+
+  self := &okta.TrustedOriginSelf{
+    Hints: hints,
+  }
+
+  links := &okta.TrustedOriginLinks{
+    Self: self,
+    Deactivate: deactivate,
+  }
+
+  trustedOrigin := &okta.TrustedOrigin{
+    Links: links,
+  }
+
+  return trustedOrigin
+}
+
+// Populates the Trusted Origin struct (used by the Okta SDK for API operaations) with the data resource provided by TF
+func populateTrustedOrigin(trustedOrigin *okta.TrustedOrigin, d *schema.ResourceData) *okta.TrustedOrigin {
+  trustedOrigin.ID = d.Get("id").(string)
+  trustedOrigin.Name = d.Get("name").(string)
+  trustedOrigin.Origin = d.Get("origin").(string)
+
+  var scopes []map[string]string
+
+  for _, vals := range d.Get("scopes").([]interface{}) {
+    scopes = append(scopes, map[string]string{"Type": vals.(string)})
+  }
+
+  trustedOrigin.Scopes = scopes
+
+  return trustedOrigin
+}
+
+func resourceTrustedOriginCreate(d *schema.ResourceData, m interface{}) error {
+  return nil
+}
+
+func resourceTrustedOriginRead(d *schema.ResourceData, m interface{}) error {
+  return nil
+}
+
+func resourceTrustedOriginUpdate(d *schema.ResourceData, m interface{}) error {
+  return nil
+}
+
+func resourceTrustedOriginDelete(d *schema.ResourceData, m interface{}) error {
+  return nil
+}
+
+// check if Trusted Origin exists in Okta
+func trustedOriginExists(d *schema.ResourceData, m interface{}) (bool, error) {
+  return true, nil
+}
---
+++ okta/trusted_origin_test.go
@@ -0,0 +1,54 @@
+package okta
+
+import (
+  "fmt"
+  "testing"
+
+  "github.com/hashicorp/terraform/helper/acctest"
+  "github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccTrustedOrigin(t *testing.T) {
+
+  // generate a random name for each widget test run, to avoid
+  // collisions from multiple concurrent tests.
+  // the acctest package includes many helpers such as RandStringFromCharSet
+  // See https://godoc.org/github.com/hashicorp/terraform/helper/acctest
+  rName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+
+  resource.Test(t, resource.TestCase{
+    PreCheck:  func() { testAccPreCheck(t) },
+    Providers: testAccProviders,
+    Steps: []resource.TestStep{
+      {
+        Config: testAccTrustedOriginCreate(rName),
+      },
+      {
+        Config: testAccTrustedOriginUpdate(rName),
+        Check: resource.ComposeTestCheckFunc(
+          resource.TestCheckResourceAttr("okta_trusted_origin.test-"+rName, "origin", "https://example2.com"),
+        ),
+      },
+    },
+  })
+}
+
+func testAccTrustedOriginCreate(name string) string {
+  return fmt.Sprintf(`
+resource "okta_trusted_origin" "test-%s" {
+  name = "%s"
+  origin = "https://example.com"
+  scopes = ["CORS"]
+}`, name, name)
+}
+
+func testAccTrustedOriginUpdate(name string) string {
+  return fmt.Sprintf(`
+resource "okta_identity_provider" "test-%s" {
+  name = "%s"
+  origin = "https://example2.com"
+  scopes = ["CORS"]
+}`, name, name)
+}
+
+
