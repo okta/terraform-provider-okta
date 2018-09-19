1a24a5312efd844af171fd8704a6c99b0fbad200
Purrbiscuit <spurr@articulate.com>
2018-09-19 16:48:50 -0400

    add to tests resource_user_test.go

--- okta/resource_user_test.go
+++ okta/resource_user_test.go
@@ -33,6 +33,34 @@ func TestAccOktaUserNew(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "first_name", "TestAcc"),
 					resource.TestCheckResourceAttr(resourceName, "last_name", rName),
 					resource.TestCheckResourceAttr(resourceName, "login", "test-acc-"+rName+"@testing.com"),
+					resource.TestCheckResourceAttr(resourceName, "email", "test1-"+rName+"@testing.com"),
+					resource.TestCheckResourceAttr(resourceName, "city", "New York"),
+					resource.TestCheckResourceAttr(resourceName, "cost_center", "10"),
+					resource.TestCheckResourceAttr(resourceName, "country_code", "US"),
+					resource.TestCheckResourceAttr(resourceName, "department", "IT"),
+					resource.TestCheckResourceAttr(resourceName, "display_name", "Dr. TestAcc "+rName),
+					resource.TestCheckResourceAttr(resourceName, "division", "Acquisitions"),
+					resource.TestCheckResourceAttr(resourceName, "employee_number", "111111"),
+					resource.TestCheckResourceAttr(resourceName, "honorific_prefix", "Dr."),
+					resource.TestCheckResourceAttr(resourceName, "honorific_suffix", "Jr."),
+					resource.TestCheckResourceAttr(resourceName, "locale", "en_US"),
+					resource.TestCheckResourceAttr(resourceName, "manager", "Jimbo"),
+					resource.TestCheckResourceAttr(resourceName, "manager_id", "222222"),
+					resource.TestCheckResourceAttr(resourceName, "middle_name", "John"),
+					resource.TestCheckResourceAttr(resourceName, "mobile_phone", "1112223333"),
+					resource.TestCheckResourceAttr(resourceName, "nick_name", "Johnny"),
+					resource.TestCheckResourceAttr(resourceName, "organization", "Testing Inc."),
+					resource.TestCheckResourceAttr(resourceName, "postal_address", "1234 Testing St."),
+					resource.TestCheckResourceAttr(resourceName, "preferred_language", "en-us"),
+					resource.TestCheckResourceAttr(resourceName, "primary_phone", "4445556666"),
+					resource.TestCheckResourceAttr(resourceName, "profile_url", "http://www.example.com/profile"),
+					resource.TestCheckResourceAttr(resourceName, "second_email", "test2-"+rName+"@testing.com"),
+					resource.TestCheckResourceAttr(resourceName, "state", "NY"),
+					resource.TestCheckResourceAttr(resourceName, "street_address", "5678 Testing Ave."),
+					resource.TestCheckResourceAttr(resourceName, "timezone", "America/New_York"),
+					resource.TestCheckResourceAttr(resourceName, "title", "Director"),
+					resource.TestCheckResourceAttr(resourceName, "user_type", "Employee"),
+					resource.TestCheckResourceAttr(resourceName, "zip_code", "11111"),
 				),
 			},
 		},
@@ -54,14 +82,40 @@ resource "okta_user" "test_acc_%s" {
 func testOktaUserConfig_updated(r string) string {
 	return fmt.Sprintf(`
 resource "okta_user" "test_acc_%s" {
-  admin_roles      = ["ORG_ADMIN"]
-  first_name       = "TestAcc"
-  last_name        = "%s"
-  login            = "test-acc-%s@testing.com"
-  honorific_prefix = "Dr."
-  honorific_suffix = "Jr."
+  admin_roles        = ["ORG_ADMIN"]
+  first_name         = "TestAcc"
+  last_name          = "%s"
+  login              = "test-acc-%s@testing.com"
+  email              = "test1-%s@testing.com"
+  city               = "New York"
+  cost_center        = "10"
+  country_code       = "US"
+  department         = "IT"
+  display_name       = "Dr. TestAcc %s"
+  division           = "Acquisitions"
+  employee_number    = "111111"
+  honorific_prefix   = "Dr."
+  honorific_suffix   = "Jr."
+  locale             = "en_US"
+  manager            = "Jimbo"
+  manager_id         = "222222"
+  middle_name        = "John"
+  mobile_phone       = "1112223333"
+  nick_name          = "Johnny"
+  organization       = "Testing Inc."
+  postal_address     = "1234 Testing St."
+  preferred_language = "en-us"
+  primary_phone      = "4445556666"
+  profile_url        = "http://www.example.com/profile"
+  second_email       = "test2-%s@testing.com"
+  state              = "NY"
+  street_address     = "5678 Testing Ave."
+  timezone           = "America/New_York"
+  title              = "Director"
+  user_type          = "Employee"
+  zip_code           = "11111"
 }
-`, r, r, r)
+`, r, r, r, r, r, r)
 }
 
 func testAccCheckUserDestroy(s *terraform.State) error {
