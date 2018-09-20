b81394bcdd7b39839e468c1786bff906b918aebb
Purrbiscuit <spurr@articulate.com>
2018-09-20 16:23:22 -0400

    alphabetize the funcs in utils.go

--- okta/utils.go
+++ okta/utils.go
@@ -7,17 +7,39 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-// The best practices states that aggregate types should have error handling (think non-primitive). This will not attempt to set nil values.
-func setNonPrimitives(data *schema.ResourceData, valueMap map[string]interface{}) error {
-	for k, v := range valueMap {
-		if v != nil {
-			if err := data.Set(k, v); err != nil {
-				return fmt.Errorf("error setting %s for resource %s: %s", k, data.Id(), err)
-			}
+// camel cased strings from okta responses become underscore separated to match
+// the terraform configs for state file setting (ie. firstName from okta response becomes first_name)
+func camelCaseToUnderscore(s string) string {
+	a := []rune(s)
+
+	for i, r := range a {
+		if !unicode.IsLower(r) {
+			a = append(a, 0)
+			a[i] = unicode.ToLower(r)
+			copy(a[i+1:], a[i:])
+			a[i] = []rune("_")[0]
 		}
 	}
 
-	return nil
+	s = string(a)
+
+	return s
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func convertBoolToInt(b bool) int {
+	if b == true {
+		return 1
+	}
+	return 0
 }
 
 func convertStringArrToInterface(stringList []string) []interface{} {
@@ -50,26 +72,6 @@ func convertIntToBool(i int) bool {
 	return false
 }
 
-func convertBoolToInt(b bool) int {
-	if b == true {
-		return 1
-	}
-	return 0
-}
-
-func contains(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
-}
-
-func suppressDefaultedArrayDiff(k, old, new string, d *schema.ResourceData) bool {
-	return new == "0"
-}
-
 func createValueDiffSuppression(newValueToIgnore string) schema.SchemaDiffSuppressFunc {
 	return func(k, old, new string, d *schema.ResourceData) bool {
 		return new == newValueToIgnore
@@ -80,33 +82,31 @@ func getClientFromMetadata(meta interface{}) *articulateOkta.Client {
 	return meta.(*Config).articulateOktaClient
 }
 
+func is404(client *articulateOkta.Client) bool {
+	return client.OktaErrorCode == "E0000007"
+}
+
+// The best practices states that aggregate types should have error handling (think non-primitive). This will not attempt to set nil values.
+func setNonPrimitives(data *schema.ResourceData, valueMap map[string]interface{}) error {
+	for k, v := range valueMap {
+		if v != nil {
+			if err := data.Set(k, v); err != nil {
+				return fmt.Errorf("error setting %s for resource %s: %s", k, data.Id(), err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func suppressDefaultedArrayDiff(k, old, new string, d *schema.ResourceData) bool {
+	return new == "0"
+}
+
 func validatePriority(in int, out int) error {
 	if in > 0 && in != out {
 		return fmt.Errorf("provided priority was not valid, got: %d, API responded with: %d. See schema for attribute details", in, out)
 	}
 
 	return nil
-}
-
-func is404(client *articulateOkta.Client) bool {
-	return client.OktaErrorCode == "E0000007"
-}
-
-// camel cased strings from okta responses become underscore separated to match
-// the terraform configs for state file setting (ie. firstName from okta response becomes first_name)
-func camelCaseToUnderscore(s string) string {
-	a := []rune(s)
-
-	for i, r := range a {
-		if !unicode.IsLower(r) {
-			a = append(a, 0)
-			a[i] = unicode.ToLower(r)
-			copy(a[i+1:], a[i:])
-			a[i] = []rune("_")[0]
-		}
-	}
-
-	s = string(a)
-
-	return s
 }
