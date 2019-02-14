e89449905e35c909c9fde66097d879f625fc5997
Andy Gertjejansen <andy@articulate.com>
2019-02-14 14:41:08 -0600

    Fix priority of policy and ensure there is no race condition wiht user

--- examples/okta_auth_server_policy/basic_updated.tf
+++ examples/okta_auth_server_policy/basic_updated.tf
@@ -2,7 +2,7 @@ resource "okta_auth_server_policy" "test" {
   status           = "ACTIVE"
   name             = "test_updated"
   description      = "test updated"
-  priority         = 2
+  priority         = 1
   client_whitelist = ["ALL_CLIENTS"]
   auth_server_id   = "${okta_auth_server.test.id}"
 }
