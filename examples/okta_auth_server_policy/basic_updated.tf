fbbaceab8c98bf1cd6d49a12e3949faefae0df86
Andy Gertjejansen <andy@articulate.com>
2019-02-14 15:00:26 -0600

    Remove priority related test

--- examples/okta_auth_server_policy/basic_updated.tf
+++ examples/okta_auth_server_policy/basic_updated.tf
@@ -7,15 +7,6 @@ resource "okta_auth_server_policy" "test" {
   auth_server_id   = "${okta_auth_server.test.id}"
 }
 
-resource "okta_auth_server_policy" "test_priority" {
-  status           = "ACTIVE"
-  name             = "other_test"
-  description      = "other test updated"
-  priority         = 1
-  client_whitelist = ["ALL_CLIENTS"]
-  auth_server_id   = "${okta_auth_server.test.id}"
-}
-
 resource "okta_auth_server" "test" {
   name        = "testAcc_%[1]d"
   description = "test"
