b63549f392743182fe7273729902d3425f361096
Andy Gertjejansen <andygertjejansen@gmail.com>
2019-05-21 15:58:16 -0500

    Fix travis build

--- examples/okta_policy_signon/basic_renamed.tf
+++ examples/okta_policy_signon/basic_renamed.tf
@@ -6,5 +6,9 @@ resource okta_policy_signon test {
   name            = "testAccUpdated_replace_with_uuid"
   status          = "ACTIVE"
   description     = "Terraform Acceptance Test SignOn Policy"
-  groups_included = ["${data.okta_group.all.id}"]
+  groups_included = ["${okta_group.test.id}"]
+}
+
+resource okta_group test {
+  name = "testAcc_replace_with_uuid"
 }
