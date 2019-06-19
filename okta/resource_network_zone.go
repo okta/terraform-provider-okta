e6ec849e35547f0e428f0c154ee8d2c327991bf2
Marco Santonastaso <m.santonastaso@protonmail.com>
2019-06-19 14:33:37 +0100

    gateways and proxies implemented

--- okta/resource_network_zone.go
+++ okta/resource_network_zone.go
@@ -2,9 +2,23 @@ package okta
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 )
+
+var addressObjectSchema = map[string]*schema.Schema{
+	"type": &schema.Schema{
+		Type:         schema.TypeString,
+		ValidateFunc: validation.StringInSlice([]string{"CIDR", "RANGE"}, false),
+		Required:     true,
+	},
+	"value": &schema.Schema{
+		Type:     schema.TypeString,
+		Required: true,
+	},
+}
 
 func resourceNetworkZone() *schema.Resource {
 	return &schema.Resource{
@@ -18,10 +32,26 @@ func resourceNetworkZone() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
+			"gateways": &schema.Schema{
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "IP addresses (range or CIDR form) of this zone",
+				Elem: &schema.Resource{
+					Schema: addressObjectSchema,
+				},
+			},
 			"name": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
 				Description: "Name of the Network Zone Resource",
+			},
+			"proxies": &schema.Schema{
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "IP addresses (range or CIDR form) allowed to forward request from",
+				Elem: &schema.Resource{
+					Schema: addressObjectSchema,
+				},
 			},
 			"type": &schema.Schema{
 				Type:         schema.TypeString,
@@ -34,6 +64,7 @@ func resourceNetworkZone() *schema.Resource {
 }
 
 func resourceNetworkZoneCreate(d *schema.ResourceData, m interface{}) error {
+	log.Printf("[INFO] Create Network Zone %v", d.Get("name").(string))
 	return fmt.Errorf("[ERROR] Okta Network Zone not implemented")
 }
 
