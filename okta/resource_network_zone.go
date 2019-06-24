7ab2874ed83ad1518f69f0ec39c1fb76064521a4
Marco Santonastaso <m.santonastaso@protonmail.com>
2019-06-24 16:59:23 +0100

    network zone resource method implemented

--- okta/resource_network_zone.go
+++ okta/resource_network_zone.go
@@ -1,9 +1,6 @@
 package okta
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/helper/validation"
 )
@@ -27,7 +24,7 @@ func resourceNetworkZone() *schema.Resource {
 				ValidateFunc: validation.StringInSlice([]string{"CIDR", "RANGE"}, false),
 			},
 			"gateway_values": {
-				Type:        schema.TypeList,
+				Type:        schema.TypeSet,
 				Optional:    true,
 				Description: "Array of values in CIDR/range form depending on the type specified",
 				Elem:        &schema.Schema{Type: schema.TypeString},
@@ -48,18 +45,78 @@ func resourceNetworkZone() *schema.Resource {
 }
 
 func resourceNetworkZoneCreate(d *schema.ResourceData, m interface{}) error {
-	log.Printf("[INFO] Create Network Zone %v", d.Get("name").(string))
-	return fmt.Errorf("[ERROR] Okta Network Zone not implemented")
+	client := getSupplementFromMetadata(m)
+	networkZone := buildNetworkZone(d, m)
+	networkZone, _, err := client.CreateNetworkZone(*networkZone, nil)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(networkZone.ID)
+	return resourceNetworkZoneRead(d, m)
 }
 
 func resourceNetworkZoneRead(d *schema.ResourceData, m interface{}) error {
-	return fmt.Errorf("[ERROR] Okta Network Zone not implemented")
+	zone, _, err := getSupplementFromMetadata(m).GetNetworkZone(d.Id())
+	if err != nil {
+		return err
+	}
+	d.Set("name", zone.Name)
+	d.Set("type", zone.Type)
+
+	return setNonPrimitives(d, map[string]interface{}{
+		// TODO
+		// "channel": flattenHookChannel(hook.Channel),
+		// "headers": flattenHeaders(hook.Channel),
+		// "auth":    flattenAuth(d, hook.Channel),
+	})
 }
 
 func resourceNetworkZoneUpdate(d *schema.ResourceData, m interface{}) error {
-	return fmt.Errorf("[ERROR] Okta Network Zone not implemented")
+	client := getSupplementFromMetadata(m)
+	networkZone := buildNetworkZone(d, m)
+	networkZone, _, err := client.UpdateNetworkZone(d.Id(), *networkZone, nil)
+
+	if err != nil {
+		return err
+	}
+
+	return resourceNetworkZoneRead(d, m)
 }
 
 func resourceNetworkZoneDelete(d *schema.ResourceData, m interface{}) error {
-	return fmt.Errorf("[ERROR] Okta Network Zone not implemented")
+	client := getSupplementFromMetadata(m)
+
+	res, err := client.DeleteNetworkZone(d.Id())
+	if err != nil {
+		return responseErr(res, err)
+	}
+
+	return err
 }
+
+func buildNetworkZone(d *schema.ResourceData, m interface{}) *NetworkZone {
+	gatewayList := []*Gateways{}
+	if values, ok := d.GetOk("gateway_values"); ok {
+		for _, value := range values.(*schema.Set).List() {
+			gatewayList = append(gatewayList, &Gateways{Type: d.Get("gateway_type").(string), Value: value.(string)})
+		}
+	}
+
+	return &NetworkZone{
+		Name:     d.Get("name").(string),
+		Type:     d.Get("type").(string),
+		Gateways: gatewayList,
+	}
+}
+
+// func buildGateways(d *schema.ResourceData, m interface{}) *Gateways {
+// 	// if _, ok := d.GetOk("gateway_type"); !ok {
+// 	// 	return nil
+// 	// }
+
+// 	return &Gateways{
+// 		Type:  getStringValue(d, "gateway_type"),
+// 		Value: getStringValue(d, "gateway_values"),
+// 	}
+// }
