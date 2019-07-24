c67632684955b301e3575126a9489a56644fc7de
Andy Gertjejansen <andygertjejansen@gmail.com>
2019-07-24 18:35:08 -0500

    Write tests around network zones

--- examples/okta_network_zone/basic_updated.tf
+++ examples/okta_network_zone/basic_updated.tf
@@ -1,12 +1,12 @@
 resource "okta_network_zone" "ip_network_zone_example" {
-  name     = "testAcc_replace_with_uuid"
+  name     = "testAcc_replace_with_uuid Updated"
   type     = "IP"
-  gateways = ["1.2.3.4/24", "2.3.4.5-2.3.4.15"]
-  proxies  = ["2.2.3.4/24", "3.3.4.5-3.3.4.15"]
+  gateways = ["1.2.3.4/24", "2.3.4.5-2.3.4.10"]
+  proxies  = ["2.2.3.4/24", "3.3.4.5-3.3.4.10"]
 }
 
 resource "okta_network_zone" "dynamic_network_zone_example" {
-  name              = "testAcc_replace_with_uuid Dynamic"
+  name              = "testAcc_replace_with_uuid Dynamic Updated"
   type              = "DYNAMIC"
   dynamic_locations = ["US", "AF-BGL"]
 }
--- okta/resource_network_zone_test.go
+++ okta/resource_network_zone_test.go
@@ -1,1 +1,51 @@
 package okta
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccNetworkZone(t *testing.T) {
+	ri := acctest.RandInt()
+	mgr := newFixtureManager(networkZone)
+	config := mgr.GetFixtures("basic.tf", ri, t)
+	updatedConfig := mgr.GetFixtures("basic_updated.tf", ri, t)
+	resourceName := fmt.Sprintf("%s.ip_network_zone_example", networkZone)
+	dynamicResourceName := fmt.Sprintf("%s.dynamic_network_zone_example", networkZone)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", fmt.Sprintf("testAcc_%d", ri)),
+					resource.TestCheckResourceAttr(resourceName, "type", "IP"),
+					resource.TestCheckResourceAttr(resourceName, "proxies.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "gateways.#", "2"),
+
+					resource.TestCheckResourceAttr(dynamicResourceName, "name", fmt.Sprintf("testAcc_%d Dynamic", ri)),
+					resource.TestCheckResourceAttr(dynamicResourceName, "type", "DYNAMIC"),
+					resource.TestCheckResourceAttr(dynamicResourceName, "dynamic_locations.#", "2"),
+				),
+			},
+			{
+				Config: updatedConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", fmt.Sprintf("testAcc_%d Updated", ri)),
+					resource.TestCheckResourceAttr(resourceName, "type", "IP"),
+					resource.TestCheckResourceAttr(resourceName, "proxies.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "gateways.#", "2"),
+
+					resource.TestCheckResourceAttr(dynamicResourceName, "name", fmt.Sprintf("testAcc_%d Dynamic Updated", ri)),
+					resource.TestCheckResourceAttr(dynamicResourceName, "type", "DYNAMIC"),
+					resource.TestCheckResourceAttr(dynamicResourceName, "dynamic_locations.#", "2"),
+				),
+			},
+		},
+	})
+}
