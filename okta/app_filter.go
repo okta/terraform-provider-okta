d427c958a34ddc9bb94a64d1e3aa6395d67db3ee
Andy Gertjejansen <andygertjejansen@gmail.com>
2019-11-27 14:57:38 -0600

    Fix old modules

--- okta/app_filter.go
+++ okta/app_filter.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/articulate/terraform-provider-okta/sdk"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/okta/okta-sdk-golang/okta"
 	"github.com/okta/okta-sdk-golang/okta/query"
+	"github.com/terraform-providers/terraform-provider-okta/sdk"
 )
 
 type (
--- okta/data_source_okta_users.go
+++ okta/data_source_okta_users.go
@@ -3,12 +3,12 @@ package okta
 import (
 	"fmt"
 
-	"github.com/terraform-providers/terraform-provider-okta/sdk"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/hashcode"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/okta/okta-sdk-golang/okta"
 	"github.com/okta/okta-sdk-golang/okta/query"
+	"github.com/terraform-providers/terraform-provider-okta/sdk"
 )
 
 func dataSourceUsers() *schema.Resource {
