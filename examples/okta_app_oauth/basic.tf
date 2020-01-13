61fb6ac535495b94dff03b0742072065cd52f183
pratikmallya <pratik.mallya@liveramp.com>
2020-01-12 17:50:15 -0800

    FIX: make examples use client_id

--- examples/okta_app_oauth/basic.tf
+++ examples/okta_app_oauth/basic.tf
@@ -5,6 +5,6 @@ resource "okta_app_oauth" "test" {
   redirect_uris              = ["http://d.com/"]
   response_types             = ["code"]
   client_basic_secret        = "something_from_somewhere"
-  custom_client_id           = "something_from_somewhere"
+  client_id           = "something_from_somewhere"
   token_endpoint_auth_method = "client_secret_basic"
 }
