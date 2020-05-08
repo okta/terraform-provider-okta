1f09fe4840e29eaa35f01481d4a6c5cbd008b64d
Justin Lewis <justin.lewis@dealer.com>
2020-05-08 14:55:46 -0400

    Just a bit more to the test terraform

--- examples/okta_app_oauth/federation_broker_on.tf
+++ examples/okta_app_oauth/federation_broker_on.tf
@@ -1,3 +1,17 @@
+resource "okta_group" "group" {
+  name = "testAcc_replace_with_uuid"
+}
+
+resource "okta_user" "user" {
+  admin_roles = ["APP_ADMIN", "USER_ADMIN"]
+  first_name  = "TestAcc"
+  last_name   = "blah"
+  login       = "test-acc-replace_with_uuid@example.com"
+  email       = "test-acc-replace_with_uuid@example.com"
+  status      = "ACTIVE"
+}
+
+
 resource "okta_app_oauth" "test" {
   label                     = "testAcc_replace_with_uuid"
   type                      = "web"
@@ -8,4 +22,14 @@ resource "okta_app_oauth" "test" {
   response_types            = ["code", "token", "id_token"]
   consent_method            = "TRUSTED"
   implicit_assignment       = true
+
+  // This block will be ignored when implicit_assignment is true,
+  // Added in so that the acceptance test would fail due to okta 400 error should that ingore code not be working
+  users {
+    id       = "${okta_user.user.id}"
+    username = "${okta_user.user.email}"
+  }
+
+  // This block will be ignored when implicit_assignment is true
+  groups = ["${okta_group.group.id}"]
 }
