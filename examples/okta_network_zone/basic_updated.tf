67bb30d60220b755c46bec389d637e169a1b575d
Praveen Atluri <vrspraveen.atluri@gmail.com>
2020-07-10 14:38:27 -0700

    updated the example file with blacklist

--- examples/okta_network_zone/basic_updated.tf
+++ examples/okta_network_zone/basic_updated.tf
@@ -1,6 +1,7 @@
 resource "okta_network_zone" "ip_network_zone_example" {
   name     = "testAcc_replace_with_uuid Updated"
   type     = "IP"
+  blacklist = false
   gateways = ["1.2.3.4/24", "2.3.4.5-2.3.4.10"]
   proxies  = ["2.2.3.4/24", "3.3.4.5-3.3.4.10"]
 }
@@ -8,5 +9,6 @@ resource "okta_network_zone" "ip_network_zone_example" {
 resource "okta_network_zone" "dynamic_network_zone_example" {
   name              = "testAcc_replace_with_uuid Dynamic Updated"
   type              = "DYNAMIC"
+  blacklist         = false
   dynamic_locations = ["US", "AF-BGL"]
 }
