59becd41dd007d0788c43e417ddf1b0193da1894
Tom Goodsell <ymylei@users.noreply.github.com>
2020-07-20 01:46:58 -0500

    Adding export comment to roles list

--- okta/roles.go
+++ okta/roles.go
@@ -1,3 +1,4 @@
 package okta
 
+// ValidAdminRoles List of all valid Okta admin roles for users and groups
 var ValidAdminRoles = []string{"SUPER_ADMIN", "ORG_ADMIN", "API_ACCESS_MANAGEMENT_ADMIN", "APP_ADMIN", "USER_ADMIN", "MOBILE_ADMIN", "READ_ONLY_ADMIN", "HELP_DESK_ADMIN"}
