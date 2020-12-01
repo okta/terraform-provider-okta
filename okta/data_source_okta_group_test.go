03a43be5a2900fef7144511d50fbeeeafe6544bd
Dan Goslen <dwgoslen@gmail.com>
2020-12-01 07:30:53 -0500

    Adding type to data okta group (#217)
    * Adding type to 'data okta_group' for both searching and as a return value
    
    Co-authored-by: Bogdan Prodan <bogdan.prodan@okta.com>

--- okta/data_source_okta_group_test.go
+++ okta/data_source_okta_group_test.go
@@ -1,6 +1,7 @@
 package okta
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -11,6 +12,7 @@ func TestAccOktaDataSourceGroup_read(t *testing.T) {
 	ri := acctest.RandInt()
 	mgr := newFixtureManager(oktaGroup)
 	config := mgr.GetFixtures("datasource.tf", ri, t)
+	configInvalid := mgr.GetFixtures("datasource_not_found.tf", ri, t)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
@@ -22,9 +24,14 @@ func TestAccOktaDataSourceGroup_read(t *testing.T) {
 				Config: config,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.okta_group.test", "id"),
+					resource.TestCheckResourceAttrSet("data.okta_group.test", "type"),
 					resource.TestCheckResourceAttrSet("okta_group.test", "id"),
 					resource.TestCheckResourceAttr("okta_group.test", "users.#", "1"),
 				),
+			},
+			{
+				Config:      configInvalid,
+				ExpectError: regexp.MustCompile(`\bwas not found with type\b`),
 			},
 		},
 	})
