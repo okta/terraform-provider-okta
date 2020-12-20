6e7c855fbbc77081d69300248623a49e80b3caf7
Tom Goodsell <ymylei@users.noreply.github.com>
2020-12-19 20:22:34 -0600

    Fix: Contains check fixup
    Modify what the contains looks for to be a bit more precise.

--- okta/resource_okta_group_membership_test.go
+++ okta/resource_okta_group_membership_test.go
@@ -2,6 +2,7 @@ package okta
 
 import (
 	"context"
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"strings"
@@ -37,7 +38,7 @@ func checkMembershipState(id string) (bool, error) {
 	client := getOktaClientFromMetadata(testAccProvider.Meta())
 	state, err := checkIfUserInGroup(context.Background(), client, groupId, userId)
 	if err != nil {
-		if strings.Contains(err.Error(), "Not Found") {
+		if strings.Contains(err.Error(), fmt.Sprintf("Resource not found: %s (UserGroup)", groupId)) {
 			return state, nil
 		}
 	}
