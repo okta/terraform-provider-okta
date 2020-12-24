82e81fcda74da31797105b2941114c385cf4d694
Tom Goodsell <ymylei@users.noreply.github.com>
2020-12-24 12:39:20 -0600

    Format test file

--- okta/resource_okta_group_role_test.go
+++ okta/resource_okta_group_role_test.go
@@ -24,19 +24,19 @@ func TestAccOktaGroupAdminRole_crud(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: config,
-				Check: resource.TestCheckResourceAttr(resourceName, "role_type", "READ_ONLY_ADMIN"),
+				Check:  resource.TestCheckResourceAttr(resourceName, "role_type", "READ_ONLY_ADMIN"),
 			},
 			{
 				Config: groupTarget,
-				Check: resource.TestCheckResourceAttrSet(resourceName, "group_target_list"),
+				Check:  resource.TestCheckResourceAttrSet(resourceName, "group_target_list"),
 			},
 			{
 				Config: groupTargetsUpdated,
-				Check: resource.TestCheckResourceAttrSet(resourceName, "group_target_list"),
+				Check:  resource.TestCheckResourceAttrSet(resourceName, "group_target_list"),
 			},
 			{
 				Config: groupTargetsRemoved,
-				Check: resource.TestCheckResourceAttr(resourceName, "role_type", "HELP_DESK_ADMIN"),
+				Check:  resource.TestCheckResourceAttr(resourceName, "role_type", "HELP_DESK_ADMIN"),
 			},
 		},
 	})
