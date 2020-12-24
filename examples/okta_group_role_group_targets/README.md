c4043713ef6d99785008c7853b4d49d899afb89a
Tom Goodsell <ymylei@users.noreply.github.com>
2020-12-23 22:39:29 -0600

    Add basic example usage
    Adds readme and basic example for group_membership_admin use.

---
+++ examples/okta_group_role_group_targets/README.md
@@ -0,0 +1,8 @@
+# okta_group_role_group_targets
+
+Represents a list of target groups for a administrator role assignment which can be scoped to groups.
+
+[See Okta documentation for more details](https://developer.okta.com/docs/reference/api/roles/#group-administrator-role-group-targets)
+
+- Example of an admin role assignment of `GROUP_MEMBERSHIP_ADMIN` for a specific set of
+  groups [can be found here](./basic.tf)
---
+++ examples/okta_group_role_group_targets/basic.tf
@@ -0,0 +1,30 @@
+// Test group & user assigned to group
+resource "okta_group" "test" {
+  name        = "testAcc_replace_with_uuid"
+  description = "testing"
+}
+
+resource "okta_group" "test_target" {
+  name        = "test_target_Acc_replace_with_uuid"
+  description = "testing"
+}
+
+resource "okta_user" "test" {
+  first_name = "TestAcc"
+  last_name  = "Smith"
+  login      = "testAcc-replace_with_uuid@example.com"
+  email      = "testAcc-replace_with_uuid@example.com"
+  group_memberships = [okta_group.test.id]
+}
+
+//Usage of role & target groups
+resource "okta_group_role" "test" {
+  group_id  = okta_group.test.id
+  role_type = "GROUP_MEMBERSHIP_ADMIN"
+}
+
+resource "okta_group_role_target_groups" "test" {
+  group_id = okta_group_role.test.group_id
+  role_id = okta_group_role.test.id
+  target_groups = [okta_group.test_target.id]
+}
