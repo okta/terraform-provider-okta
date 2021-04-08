0a2f9f149d14bd7cc7918c56c7fb5fa055242d5f
Tom Goodsell <ymylei@users.noreply.github.com>
2021-04-07 21:07:58 -0500

    convert group membership resource to work with sets of group ids

--- okta/resource_okta_user_group_memberships.go
+++ okta/resource_okta_user_group_memberships.go
@@ -1,4 +1,4 @@
-ackage okta
+package okta
 
 import (
 	"context"
@@ -9,19 +9,18 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/okta/okta-sdk-golang/v2/okta"
-	"github.com/okta/okta-sdk-golang/v2/okta/query"
 )
 
-func resourceGroupMembership() *schema.Resource {
+func resourceUserGroupMemberships() *schema.Resource {
 	return &schema.Resource{
-		CreateContext: resourceGroupMembershipCreate,
-		ReadContext:   resourceGroupMembershipRead,
-		UpdateContext: nil,
-		DeleteContext: resourceGroupMembershipDelete,
+		CreateContext: resourceUserGroupMembershipCreate,
+		ReadContext:   resourceUserGroupMembershipRead,
+		UpdateContext: resourceUserGroupMembershipUpdate,
+		DeleteContext: resourceUserGroupMembershipDelete,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
-		Description: "",
+		Description: "Resource to manage a set of group memberships for a specific user.",
 		Schema: map[string]*schema.Schema{
 			"user_id": {
 				Type:        schema.TypeString,
@@ -29,71 +28,144 @@ func resourceGroupMembership() *schema.Resource {
 				Description: "ID of a Okta User",
 				ForceNew:    true,
 			},
-			"group_id": {
-				Type:        schema.TypeString,
+			"groups": {
+				Type:        schema.TypeSet,
 				Required:    true,
-				Description: "ID of a Okta Group",
-				ForceNew:    true,
+				Description: "The list of Okta group IDs which the user should have membership managed for.",
+				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
 		},
 	}
 }
 
-func resourceGroupMembershipCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	groupId := d.Get("group_id").(string)
+func resourceUserGroupMembershipCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	userId := d.Get("user_id").(string)
-	logger(m).Info("adding user to group", "group", groupId, "user", userId)
+	groups := convertInterfaceToStringSetNullable(d.Get("groups"))
 	client := getOktaClientFromMetadata(m)
-	_, err := client.Group.AddUserToGroup(ctx, groupId, userId)
+	err := addUserToGroups(ctx, client, userId, groups)
 	if err != nil {
-		return diag.Errorf("failed to add user to group: %v", err)
+		return diag.FromErr(err)
 	}
 	bOff := backoff.NewExponentialBackOff()
 	bOff.MaxElapsedTime = time.Second * 10
 	bOff.InitialInterval = time.Second
 	err = backoff.Retry(func() error {
-		inGroup, err := checkIfUserInGroup(ctx, client, groupId, userId)
+		ok, err := checkIfUserHasGroups(ctx, client, userId, groups)
+		//TODO: Fix error messages
 		if err != nil {
-			return backoff.Permanent(fmt.Errorf("failed to find user (%s) in group (%s) after addition with error: %v", userId, groupId, err))
+			return backoff.Permanent(err)
 		}
-		if inGroup {
+		if ok {
 			return nil
 		}
-		return fmt.Errorf("failed to find user (%s) in group (%s) after multiple tries", userId, groupId)
+		return fmt.Errorf("user (%s) did not have expected group memberships after multiple checks", userId)
 	}, bOff)
 	if err != nil {
 		return diag.FromErr(err)
 	}
-	d.SetId(fmt.Sprintf("%s+%s", groupId, userId))
+	d.SetId(userId)
 	return nil
 }
 
-func resourceGroupMembershipRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	groupId := d.Get("group_id").(string)
+func resourceUserGroupMembershipRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	userId := d.Get("user_id").(string)
-	logger(m).Info("checking for membership in group", "group", groupId, "user", userId)
+	groups := convertInterfaceToStringSetNullable(d.Get("groups"))
 	client := getOktaClientFromMetadata(m)
-	inGroup, err := checkIfUserInGroup(ctx, client, groupId, userId)
+	ok, err := checkIfUserHasGroups(ctx, client, userId, groups)
 	if err != nil {
 		return diag.Errorf("unable to complete group check for user: %v", err)
 	}
-	if inGroup {
+	if ok {
 		return nil
 	} else {
 		d.SetId("")
-		logger(m).Info("user is not in group", "group", groupId, "user", userId)
+		logger(m).Info("user (%s) did not have expected group memberships", userId)
 		return nil
 	}
 }
 
-func resourceGroupMembershipDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	groupId := d.Get("group_id").(string)
+func resourceUserGroupMembershipDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	userId := d.Get("user_id").(string)
-	logger(m).Info("removing user from group", "group", groupId, "user", userId)
+	groups := convertInterfaceToStringSetNullable(d.Get("groups"))
 	client := getOktaClientFromMetadata(m)
-	_, err := client.Group.RemoveUserFromGroup(ctx, groupId, userId)
+	err := removeUserFromGroups(ctx, client, userId, groups)
 	if err != nil {
-		return diag.Errorf("failed to remove user to group: %v", err)
+		return diag.FromErr(err)
+	}
+	return nil
+}
+
+func resourceUserGroupMembershipUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	userId := d.Get("user_id").(string)
+	client := getOktaClientFromMetadata(m)
+
+	old, new := d.GetChange("groups")
+
+	oldSet := old.(*schema.Set)
+	newSet := new.(*schema.Set)
+
+	groupsToAdd := convertInterfaceToStringSetNullable(newSet.Difference(oldSet).List())
+	groupsToRemove := convertInterfaceToStringSetNullable(oldSet.Difference(newSet).List())
+
+	err := addUserToGroups(ctx, client, userId, groupsToAdd)
+	if err != nil {
+		diag.FromErr(err)
+	}
+
+	err = removeUserFromGroups(ctx, client, userId, groupsToRemove)
+	if err != nil {
+		diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func checkIfUserHasGroups(ctx context.Context, client *okta.Client, userId string, groups []string) (bool, error) {
+	userGroups, _, err := client.User.ListUserGroups(ctx, userId)
+	if err != nil {
+		return false, fmt.Errorf("unable to return groups for user (%s) from API", userId)
+	}
+
+	// Create set of groups
+	expectedGroupSet := make(map[string]bool)
+
+	for _, group := range groups {
+		expectedGroupSet[group] = false
+	}
+
+	// Use groups pulled from user and mark set if found
+	for _, group := range userGroups {
+		if _, ok := expectedGroupSet[group.Id]; ok {
+			expectedGroupSet[group.Id] = true
+		}
+	}
+
+	// Check set for any missing values
+	for _, state := range expectedGroupSet {
+		if !state {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+func addUserToGroups(ctx context.Context, client *okta.Client, userId string, groups []string) error {
+	for _, group := range groups {
+		_, err := client.Group.AddUserToGroup(ctx, group, userId)
+		if err != nil {
+			return fmt.Errorf("failed to add user (%s) to group (%s): %v", userId, group, err)
+		}
+	}
+	return nil
+}
+
+func removeUserFromGroups(ctx context.Context, client *okta.Client, userId string, groups []string) error {
+	for _, group := range groups {
+		_, err := client.Group.RemoveUserFromGroup(ctx, group, userId)
+		if err != nil {
+			return fmt.Errorf("failed to remove user (%s) from group (%s): %v", userId, group, err)
+		}
 	}
 	return nil
 }
