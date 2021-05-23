900a1d4ac9ccfaea5a348f1e098a6d737d298589
Tom Goodsell <ymylei@users.noreply.github.com>
2021-05-23 18:06:59 -0500

    add create function and util functions similar to okta_app_group_assignments

--- okta/resource_okta_app_user_assignments.go
+++ okta/resource_okta_app_user_assignments.go
@@ -1,10 +1,15 @@
 package okta
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/okta/okta-sdk-golang/v2/okta"
+	"github.com/okta/okta-sdk-golang/v2/okta/query"
 )
 
 func resourceOktaAppUserAssignments() *schema.Resource {
@@ -24,9 +29,20 @@ func resourceOktaAppUserAssignments() *schema.Resource {
 				Type:        schema.TypeSet,
 				Required:    true,
 				Description: "Set of users to associate with the app",
+				MinItems:    1,
+				Set: func(v interface{}) int {
+					buf := bytes.NewBuffer(nil)
+					user := v.(map[string]interface{})
+
+					buf.WriteString(fmt.Sprintf("%s-", user["id"].(string)))
+					buf.WriteString(fmt.Sprintf("%s-", user["username"].(string)))
+					buf.WriteString(fmt.Sprintf("%s-", normalizeDataJSON(user["profile"])))
+
+					return schema.HashString(buf.String())
+				},
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
-						"user_id": {
+						"id": {
 							Type:        schema.TypeString,
 							Required:    true,
 							Description: "User associated with the application",
@@ -63,5 +79,71 @@ func resourceOktaAppUserAssignments() *schema.Resource {
 }
 
 func resourceOktaAppUserAssignmentsCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	return diag.Errorf("not implemented")
+	users := d.Get("users").(*schema.Set).List()
+	client := getOktaClientFromMetadata(m)
+	appID := d.Get("app_id").(string)
+
+	assignments := tfUsersToUserAssignments(users...)
+
+	err := addUserAssignments(ctx, client, appID, assignments)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	//okta_app_user_assignments completely controls all assignments for an application
+	d.SetId(appID)
+	return nil //TODO: Use read function here
+}
+
+func tfUsersToUserAssignments(users ...interface{}) map[string]okta.AppUser {
+	assignments := map[string]okta.AppUser{}
+
+	for _, rawUser := range users {
+		user := rawUser.(map[string]interface{})
+
+		id := user["id"].(string)
+		if id == "" {
+			continue
+		}
+
+		username := user["username"].(string)
+
+		password := user["password"].(string)
+
+		rawProfile := user["profile"]
+		var profile interface{}
+		_ = json.Unmarshal([]byte(rawProfile.(string)), &profile)
+
+		assignments[id] = okta.AppUser{
+			Id:      id,
+			Profile: profile,
+			Credentials: &okta.AppUserCredentials{
+				UserName: username,
+				Password: &okta.AppUserPasswordCredential{
+					Value: password,
+				},
+			},
+		}
+	}
+	return assignments
+}
+
+func addUserAssignments(ctx context.Context, client *okta.Client, appID string, assignments map[string]okta.AppUser) error {
+	for userID, assignment := range assignments {
+		_, _, err := client.Application.AssignUserToApplication(ctx, appID, assignment)
+		if err != nil {
+			return fmt.Errorf("failed to assign user (%s) to app (%s): %s", userID, appID, err)
+		}
+	}
+	return nil
+}
+
+func removeUserAssignments(ctx context.Context, client *okta.Client, appID string, assignments map[string]okta.AppUser) error {
+	for userID, _ := range assignments {
+		_, err := client.Application.DeleteApplicationUser(ctx, appID, userID, &query.Params{})
+		if err != nil {
+			return fmt.Errorf("failed to unassign user (%s) from app (%s): %s", userID, appID, err)
+		}
+	}
+	return nil
 }
