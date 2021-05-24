87ad51952efb37b95572b338dca8385f891408a4
Tom Goodsell <ymylei@users.noreply.github.com>
2021-05-23 20:16:50 -0500

    add simple checks

--- okta/resource_okta_app_user_assignments_test.go
+++ okta/resource_okta_app_user_assignments_test.go
@@ -15,9 +15,9 @@ func TestAccAppUserAssignments_crud(t *testing.T) {
 	config := mgr.GetFixtures("basic.tf", ri, t)
 	updatedConfig := mgr.GetFixtures("updated.tf", ri, t)
 
-	user1 := fmt.Sprintf("%s.test1", oktaUser)
-	user2 := fmt.Sprintf("%s.test2", oktaUser)
-	user3 := fmt.Sprintf("%s.test3", oktaUser)
+	// user1 := fmt.Sprintf("%s.test1", oktaUser)
+	// user2 := fmt.Sprintf("%s.test2", oktaUser)
+	// user3 := fmt.Sprintf("%s.test3", oktaUser)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
@@ -26,12 +26,21 @@ func TestAccAppUserAssignments_crud(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "app_id"),
+				),
 			},
 			{
 				Config: updatedConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "app_id"),
+				),
 			},
 			{
 				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "app_id"),
+				),
 			},
 		},
 	})
