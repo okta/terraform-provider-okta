859901d0df40c5406d78b402da3d5bbd2a35c4c2
Tom Goodsell <ymylei@users.noreply.github.com>
2021-05-30 21:59:03 -0500

    create base data source

---
+++ okta/data_source_okta_app_group_assignments.go
@@ -0,0 +1,46 @@
+package okta
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/okta/okta-sdk-golang/v2/okta/query"
+)
+
+func dataSourceOktaAppGroupAssignments() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: nil,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "ID of the Okta App being queried for groups",
+				ForceNew:    true,
+			},
+			"groups": {
+				Type:        schema.TypeSet,
+				Computed:    true,
+				Elem:        schema.TypeString,
+				Description: "List of groups IDs assigned to the app",
+			},
+		},
+	}
+}
+
+func dataSourceOktaAppGroupAssignmentsRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := getOktaClientFromMetadata(m)
+	id := d.Get("id").(string)
+
+	groupAssignments, _, err := client.Application.ListApplicationGroupAssignments(ctx, id, &query.Params{})
+	if err != nil {
+		return diag.Errorf("Unable to query for groups from app (%s): %s", id, err)
+	}
+
+	var groups []string
+	for _, assignment := range groupAssignments {
+		groups = append(groups, assignment.Id)
+	}
+	_ = d.Set("groups", convertStringSetToInterface(groups))
+	return nil
+}
