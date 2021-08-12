2d68e4cba070c12d4a9b2088ed85a709660e28e5
Mike Mondragon <mikemondragon@gmail.com>
2021-08-11 22:10:24 -0700

    Update transport with logging and api mutex changes.

--- okta/internal/transport/governed_transport_test.go
+++ okta/internal/transport/governed_transport_test.go
@@ -3,19 +3,17 @@ package transport
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"testing"
 	"time"
 
+	"github.com/hashicorp/go-hclog"
+
 	"github.com/okta/terraform-provider-okta/okta/internal/apimutex"
 )
 
 func TestPreRequestHook(t *testing.T) {
-	log.SetOutput(ioutil.Discard)
-
 	percentage := 10
 	limit := 25
 	remaining := 23
@@ -24,30 +22,28 @@ func TestPreRequestHook(t *testing.T) {
 
 	client := &http.Client{}
 	apiMutex, _ := apimutex.NewAPIMutex(percentage)
-	transport := NewGovernedTransport(client.Transport, apiMutex)
+	transport := NewGovernedTransport(client.Transport, apiMutex, hclog.NewNullLogger())
 
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
 
-	apiMutex.Update(path, limit, remaining, reset)
-	if err := transport.preRequestHook(ctx, path); err != nil {
+	apiMutex.Update(http.MethodGet, path, limit, remaining, reset)
+	if err := transport.preRequestHook(ctx, http.MethodGet, path); err != nil {
 		t.Errorf("Didn't expect error, got %+v", err)
 	}
 
 	remaining--
-	apiMutex.Update(path, limit, remaining, reset)
-	if err := transport.preRequestHook(ctx, path); err != context.Canceled {
+	apiMutex.Update(http.MethodGet, path, limit, remaining, reset)
+	if err := transport.preRequestHook(ctx, http.MethodGet, path); err != context.Canceled {
 		t.Errorf("Expected %v error, got %+v", context.Canceled, err)
 	}
 }
 
 func TestPostRequestHook(t *testing.T) {
-	log.SetOutput(ioutil.Discard)
-
 	percentage := 10
 	client := &http.Client{}
 	apiMutex, _ := apimutex.NewAPIMutex(percentage)
-	transport := NewGovernedTransport(client.Transport, apiMutex)
+	transport := NewGovernedTransport(client.Transport, apiMutex, hclog.NewNullLogger())
 
 	path := "/api/v1/apps"
 	request := http.Request{
@@ -67,8 +63,8 @@ func TestPostRequestHook(t *testing.T) {
 		Header:  headers,
 	}
 
-	transport.postRequestHook(path, &response)
-	status := apiMutex.Status(path)
+	transport.postRequestHook(http.MethodGet, path, &response)
+	status := apiMutex.Status(http.MethodGet, path)
 	if status.Reset() != reset || status.Limit() != limit || status.Remaining() != remaining {
 		t.Fatalf("expected %q api mutex status %+v to have reset %d, limit %d, and remaining %d values", path, status, reset, limit, remaining)
 	}
