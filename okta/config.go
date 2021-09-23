44024965c2cc0acd3e9c604d72901904324ae691
Bogdan Prodan <bogdan.prodan@okta.com>
2021-09-23 20:16:38 +0300

    Added changelog

--- okta/config.go
+++ okta/config.go
@@ -100,7 +100,7 @@ func (c *Config) loadAndValidate(ctx context.Context) error {
 		okta.WithRateLimitMaxBackOff(int64(c.maxWait)),
 		okta.WithRequestTimeout(int64(c.requestTimeout)),
 		okta.WithRateLimitMaxRetries(int32(c.retryCount)),
-		okta.WithUserAgentExtra("okta-terraform/3.13.12"),
+		okta.WithUserAgentExtra("okta-terraform/3.13.13"),
 	}
 	if c.apiToken == "" {
 		setters = append(setters, okta.WithAuthorizationMode("PrivateKey"))
