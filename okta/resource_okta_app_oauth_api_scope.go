okta/terraform-provider-okta

commit faf33c823ecc911f08b6f4e179de0dfdf2dea7d4

Update supported scopes (#712)
This aligns the scopes with the supported list provided via https://developer.okta.com/docs/guides/implement-oauth-for-okta/scopes/#scope-naming.

okta/resource_okta_app_oauth_api_scope.go
@@ -10,10 +10,13 @@ import (
 	"github.com/okta/okta-sdk-golang/v2/okta"
 )
 
+# validScopes is a list of supported scopes as per https://developer.okta.com/docs/guides/implement-oauth-for-okta/scopes/.
 var validScopes = []string{
 	"okta.apps.manage", "okta.apps.read",
 	"okta.authorizationServers.manage", "okta.authorizationServers.read",
 	"okta.clients.manage", "okta.clients.read", "okta.clients.register",
+	"okta.devices.manage", "okta.devices.read",
+	"okta.domains.manage", "okta.domains.read",
 	"okta.eventHooks.manage", "okta.eventHooks.read",
 	"okta.events.read",
 	"okta.factors.manage", "okta.factors.read",

commit c47c00656c859a544a9c9f3ede8cb757fdbffe75

Use a correct comment type
Fixes the error `invalid character U+0023 '#'` as spotted by @monde 

This issue was introduced in https://github.com/okta/terraform-provider-okta/pull/712 and sadly not caught by a linter in the CI.

okta/resource_okta_app_oauth_api_scope.go
@@ -10,7 +10,7 @@ import (
 	"github.com/okta/okta-sdk-golang/v2/okta"
 )
 
-# validScopes is a list of supported scopes as per https://developer.okta.com/docs/guides/implement-oauth-for-okta/scopes/.
+// validScopes is a list of supported scopes as per https://developer.okta.com/docs/guides/implement-oauth-for-okta/scopes/.
 var validScopes = []string{
 	"okta.apps.manage", "okta.apps.read",
 	"okta.authorizationServers.manage", "okta.authorizationServers.read",