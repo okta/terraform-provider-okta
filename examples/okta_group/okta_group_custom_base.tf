9c14ddef3273988a0a9c7af01159e970569e1be6
Tom Goodsell <ymylei@users.noreply.github.com>
2021-12-09 19:58:08 -0600

    feat(okta_group): create test files for custom_profile_properties

---
+++ examples/okta_group/okta_group_custom_base.tf
@@ -0,0 +1,30 @@
+resource "okta_group" "test" {
+  name        = "testAcc"
+  description = "testing, testing"
+
+}
+
+// Test Schema
+resource "okta_group_schema_property" "test1" {
+  index       = "testSchema1"
+  title       = "TestSchema1"
+  type        = "string"
+  description = "Test string schema"
+  master      = "OKTA"
+}
+
+resource "okta_group_schema_property" "test2" {
+  index       = "testSchema2"
+  title       = "TestSchema2"
+  type        = "bool"
+  description = "Test bool schema"
+  master      = "OKTA"
+}
+
+resource "okta_group_schema_property" "test3" {
+  index       = "testSchema3"
+  title       = "TestSchema3"
+  type        = "number"
+  description = "Test bool schema"
+  master      = "OKTA"
+}
