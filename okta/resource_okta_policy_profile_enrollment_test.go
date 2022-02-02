b8d53bd38aee39a81c2eaf861b27d968f66f2633
Mike Mondragon <mikemondragon@gmail.com>
2022-02-02 15:51:50 -0800

    Clean ups found with `make vet`

--- okta/resource_okta_policy_profile_enrollment_test.go
+++ okta/resource_okta_policy_profile_enrollment_test.go
@@ -6,12 +6,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/okta/terraform-provider-okta/sdk"
 )
-
-func deleteProfileEnrollmentPolicies(client *testClient) error {
-	return deletePolicyByType(sdk.ProfileEnrollmentPolicyType, client)
-}
 
 func TestAccOktaPolicyProfileEnrollment(t *testing.T) {
 	ri := acctest.RandInt()
