355eddacf332f60fd3dbd8d150e31dbeb9445a9c
Bala Ganaparthi <bala.ganaparthi@okta.com>
2022-04-12 21:17:18 -0500

    Fix : Issue#1060 remove old_password requirement when setting user's credential object

--- okta/resource_okta_user.go
+++ okta/resource_okta_user.go
@@ -599,8 +599,8 @@ func resourceUserUpdate(ctx context.Context, d *schema.ResourceData, m interface
 
 	if passwordChange {
 		oldPassword, newPassword := d.GetChange("password")
-		old, ok := d.GetOk("old_password")
-		if ok {
+		old, oldPasswordExist := d.GetOk("old_password")
+		if oldPasswordExist {
 			oldPassword = old
 		}
 		op := &okta.PasswordCredential{
@@ -609,13 +609,29 @@ func resourceUserUpdate(ctx context.Context, d *schema.ResourceData, m interface
 		np := &okta.PasswordCredential{
 			Value: newPassword.(string),
 		}
-		npr := &okta.ChangePasswordRequest{
-			OldPassword: op,
-			NewPassword: np,
+		if oldPasswordExist {
+			npr := &okta.ChangePasswordRequest{
+				OldPassword: op,
+				NewPassword: np,
+			}
+			_, _, err := client.User.ChangePassword(ctx, d.Id(), *npr, nil)
+			if err != nil {
+				return diag.Errorf("failed to update user's password: %v", err)
+			}
 		}
-		_, _, err := client.User.ChangePassword(ctx, d.Id(), *npr, nil)
-		if err != nil {
-			return diag.Errorf("failed to update user's password: %v", err)
+		if !oldPasswordExist {
+			password, _ := newPassword.(string)
+			user := okta.User{
+				Credentials: &okta.UserCredentials{
+					Password: &okta.PasswordCredential{
+						Value: password,
+					},
+				},
+			}
+			_, _, err := client.User.UpdateUser(ctx, d.Id(), user, nil)
+			if err != nil {
+				return diag.Errorf("failed to set user's password: %v", err)
+			}
 		}
 	}
 
