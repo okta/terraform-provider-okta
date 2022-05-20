okta/terraform-provider-okta

commit fcfdf81d24056847a90a01f3ea00c69349832882

Allow user lookup by group membership

okta/group.go
@@ -8,16 +8,14 @@ import (
 	"github.com/okta/okta-sdk-golang/v2/okta/query"
 )
 
-func listGroupUserIDs(ctx context.Context, m interface{}, id string) ([]string, error) {
-	var resUsers []string
+func listGroupUsers(ctx context.Context, m interface{}, id string) ([]*okta.User, error) {
+	var resUsers []*okta.User
 	users, resp, err := getOktaClientFromMetadata(m).Group.ListGroupUsers(ctx, id, &query.Params{Limit: defaultPaginationLimit})
 	if err != nil {
 		return nil, err
 	}
 	for {
-		for _, user := range users {
-			resUsers = append(resUsers, user.Id)
-		}
+		resUsers = append(resUsers, users...)
 		if resp.HasNextPage() {
 			resp, err = resp.Next(ctx, &users)
 			if err != nil {
@@ -27,6 +25,18 @@ func listGroupUserIDs(ctx context.Context, m interface{}, id string) ([]string, 
 		} else {
 			break
 		}
+	}
+	return resUsers, nil
+}
+
+func listGroupUserIDs(ctx context.Context, m interface{}, id string) ([]string, error) {
+	var resUsers []string
+	users, err := listGroupUsers(ctx, m, id)
+	if err != nil {
+		return nil, err
+	}
+	for _, user := range users {
+		resUsers = append(resUsers, user.Id)
 	}
 	return resUsers, nil
 }

examples/okta_users/group.tf
@@ -1,0 +1,43 @@
+data "okta_users" "test" {
+  group_id = okta_group.test.id
+}
+
+resource "okta_group" "test" {
+  name = "foo_replace_with_uuid"
+}
+
+resource "okta_user" "test" {
+  first_name = "TestAcc"
+  last_name  = "Jones"
+  login      = "john_replace_with_uuid@ledzeppelin.com"
+  email      = "john_replace_with_uuid@ledzeppelin.com"
+}
+
+resource "okta_user" "test1" {
+  first_name = "TestAcc"
+  last_name  = "Entwhistle"
+  login      = "john_replace_with_uuid@thewho.com"
+  email      = "john_replace_with_uuid@thewho.com"
+}
+
+resource "okta_user" "test2" {
+  first_name = "TestAcc"
+  last_name  = "Doe"
+  login      = "john_replace_with_uuid@unknown.com"
+  email      = "john_replace_with_uuid@unknown.com"
+}
+
+resource "okta_user" "test3" {
+  first_name = "TestAcc"
+  last_name  = "Astley"
+  login      = "rick_astley_replace_with_uuid@rickrollin.com"
+  email      = "rick_astley_replace_with_uuid@rickrollin.com"
+}
+
+resource "okta_group_memberships" "test" {
+  group_id = okta_group.test.id
+  users = [
+    okta_user.test.id,
+    okta_user.test1.id
+  ]
+}

examples/okta_users/group_with_groups.tf
@@ -1,0 +1,44 @@
+data "okta_users" "test" {
+  group_id       = okta_group.test.id
+  include_groups = true
+}
+
+resource "okta_group" "test" {
+  name = "foo_replace_with_uuid"
+}
+
+resource "okta_user" "test" {
+  first_name = "TestAcc"
+  last_name  = "Jones"
+  login      = "john_replace_with_uuid@ledzeppelin.com"
+  email      = "john_replace_with_uuid@ledzeppelin.com"
+}
+
+resource "okta_user" "test1" {
+  first_name = "TestAcc"
+  last_name  = "Entwhistle"
+  login      = "john_replace_with_uuid@thewho.com"
+  email      = "john_replace_with_uuid@thewho.com"
+}
+
+resource "okta_user" "test2" {
+  first_name = "TestAcc"
+  last_name  = "Doe"
+  login      = "john_replace_with_uuid@unknown.com"
+  email      = "john_replace_with_uuid@unknown.com"
+}
+
+resource "okta_user" "test3" {
+  first_name = "TestAcc"
+  last_name  = "Astley"
+  login      = "rick_astley_replace_with_uuid@rickrollin.com"
+  email      = "rick_astley_replace_with_uuid@rickrollin.com"
+}
+
+resource "okta_group_memberships" "test" {
+  group_id = okta_group.test.id
+  users = [
+    okta_user.test.id,
+    okta_user.test1.id
+  ]
+}

examples/okta_users/users_with_group.tf
@@ -1,0 +1,39 @@
+resource "okta_group" "test" {
+  name = "foo_replace_with_uuid"
+}
+
+resource "okta_user" "test" {
+  first_name = "TestAcc"
+  last_name  = "Jones"
+  login      = "john_replace_with_uuid@ledzeppelin.com"
+  email      = "john_replace_with_uuid@ledzeppelin.com"
+}
+
+resource "okta_user" "test1" {
+  first_name = "TestAcc"
+  last_name  = "Entwhistle"
+  login      = "john_replace_with_uuid@thewho.com"
+  email      = "john_replace_with_uuid@thewho.com"
+}
+
+resource "okta_user" "test2" {
+  first_name = "TestAcc"
+  last_name  = "Doe"
+  login      = "john_replace_with_uuid@unknown.com"
+  email      = "john_replace_with_uuid@unknown.com"
+}
+
+resource "okta_user" "test3" {
+  first_name = "TestAcc"
+  last_name  = "Astley"
+  login      = "rick_astley_replace_with_uuid@rickrollin.com"
+  email      = "rick_astley_replace_with_uuid@rickrollin.com"
+}
+
+resource "okta_group_memberships" "test" {
+  group_id = okta_group.test.id
+  users = [
+    okta_user.test.id,
+    okta_user.test1.id
+  ]
+}

okta/data_source_okta_users.go
@@ -15,9 +15,20 @@ func dataSourceUsers() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceUsersRead,
 		Schema: map[string]*schema.Schema{
+			"group_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Find users based on group membership using the id of the group.",
+			},
+			"include_groups": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Fetch group memberships for each user",
+			},
 			"search": {
 				Type:        schema.TypeSet,
-				Required:    true,
+				Optional:    true,
 				Description: "Filter to find a user, each filter will be concatenated with an AND clause. Please be aware profile properties must match what is in Okta, which is likely camel case",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -57,16 +68,41 @@ func dataSourceUsers() *schema.Resource {
 }
 
 func dataSourceUsersRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	params := &query.Params{Search: getSearchCriteria(d), Limit: defaultPaginationLimit, SortOrder: "0"}
-	users, err := collectUsers(ctx, getOktaClientFromMetadata(m), params)
+	var (
+		users []*okta.User
+		id    string
+		err   error
+	)
+
+	client := getOktaClientFromMetadata(m)
+
+	if groupId, ok := d.GetOk("group_id"); ok {
+		id = groupId.(string)
+		users, err = listGroupUsers(ctx, m, id)
+	} else if _, ok := d.GetOk("search"); ok {
+		params := &query.Params{Search: getSearchCriteria(d), Limit: defaultPaginationLimit, SortOrder: "0"}
+		id = fmt.Sprintf("%d", crc32.ChecksumIEEE([]byte(params.String())))
+		users, err = collectUsers(ctx, client, params)
+	} else {
+		return diag.Errorf("must specify either group_id or search attributes")
+	}
+
 	if err != nil {
 		return diag.Errorf("failed to list users: %v", err)
 	}
-	d.SetId(fmt.Sprintf("%d", crc32.ChecksumIEEE([]byte(params.String()))))
+	d.SetId(id)
+	shouldGetGroups := d.Get("include_groups").(bool)
 	arr := make([]map[string]interface{}, len(users))
 	for i, user := range users {
 		rawMap := flattenUser(user)
 		rawMap["id"] = user.Id
+		if shouldGetGroups {
+			groups, err := getGroupsForUser(ctx, user.Id, client)
+			if err != nil {
+				return diag.Errorf("failed to list users: %v", err)
+			}
+			rawMap["group_memberships"] = groups
+		}
 		arr[i] = rawMap
 	}
 	_ = d.Set("users", arr)

okta/data_source_okta_users_test.go
@@ -39,3 +39,73 @@ func TestAccOktaDataSourceUsers_read(t *testing.T) {
 		},
 	})
 }
+
+func TestAccOktaDataSourceUsers_readWithGroupId(t *testing.T) {
+	ri := acctest.RandInt()
+	mgr := newFixtureManager(users)
+	users := mgr.GetFixtures("users_with_group.tf", ri, t)
+	config := mgr.GetFixtures("group.tf", ri, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: testAccProvidersFactories,
+		Steps: []resource.TestStep{
+			{
+				// Ensure user and group are created
+				Config: users,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("okta_user.test", "id"),
+					resource.TestCheckResourceAttrSet("okta_user.test1", "id"),
+					resource.TestCheckResourceAttrSet("okta_user.test2", "id"),
+					resource.TestCheckResourceAttrSet("okta_user.test3", "id"),
+					resource.TestCheckResourceAttrSet("okta_group.test", "id"),
+					resource.TestCheckResourceAttr("okta_group_memberships.test", "users.#", "2"),
+				),
+			},
+			{
+				// Ensure data source props are set
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.okta_users.test", "users.#", "2"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccOktaDataSourceUsers_readWithGroupIdIncludingGroups(t *testing.T) {
+	ri := acctest.RandInt()
+	mgr := newFixtureManager(users)
+	users := mgr.GetFixtures("users_with_group.tf", ri, t)
+	config := mgr.GetFixtures("group_with_groups.tf", ri, t)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: testAccProvidersFactories,
+		Steps: []resource.TestStep{
+			{
+				// Ensure user and group are created
+				Config: users,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("okta_user.test", "id"),
+					resource.TestCheckResourceAttrSet("okta_user.test1", "id"),
+					resource.TestCheckResourceAttrSet("okta_user.test2", "id"),
+					resource.TestCheckResourceAttrSet("okta_user.test3", "id"),
+					resource.TestCheckResourceAttrSet("okta_group.test", "id"),
+					resource.TestCheckResourceAttr("okta_group_memberships.test", "users.#", "2"),
+				),
+			},
+			{
+				// Ensure data source props are set
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.okta_users.test", "users.#", "2"),
+				),
+			},
+		},
+	})
+}

okta/user.go
@@ -289,14 +289,13 @@ func setAdminRoles(ctx context.Context, d *schema.ResourceData, m interface{}) e
 	})
 }
 
-// set all groups currently attached to the user
-func setAllGroups(ctx context.Context, d *schema.ResourceData, c *okta.Client) error {
-	groups, response, err := c.User.ListUserGroups(ctx, d.Id())
+func getGroupsForUser(ctx context.Context, id string, c *okta.Client) ([]string, error) {
+	groups, response, err := c.User.ListUserGroups(ctx, id)
 	if err != nil {
-		return fmt.Errorf("failed to list user groups: %v", err)
+		return nil, fmt.Errorf("failed to list user groups: %v", err)
 	}
 
-	groupIDs := make([]interface{}, 0)
+	groupIDs := make([]string, 0)
 
 	for {
 		for _, group := range groups {
@@ -310,12 +309,22 @@ func setAllGroups(ctx context.Context, d *schema.ResourceData, c *okta.Client) e
 		response, err = response.Next(ctx, &groups)
 
 		if err != nil {
-			return fmt.Errorf("failed to list user groups: %v", err)
+			return nil, fmt.Errorf("failed to list user groups: %v", err)
 		}
 	}
 
+	return groupIDs, nil
+}
+
+// set all groups currently attached to the user
+func setAllGroups(ctx context.Context, d *schema.ResourceData, c *okta.Client) error {
+	groupIDs, err := getGroupsForUser(ctx, d.Id(), c)
+	if err != nil {
+		return err
+	}
+	gids := convertStringSliceToInterfaceSlice(groupIDs)
 	return setNonPrimitives(d, map[string]interface{}{
-		"group_memberships": schema.NewSet(schema.HashString, groupIDs),
+		"group_memberships": schema.NewSet(schema.HashString, gids),
 	})
 }
 

commit e5d991b26147e624fed1c5b1c880d9e7b21c0206

Correct very subtle pagination bug introduced by fcfdf81d24056847a90a01f3ea00c69349832882 Closes #1119

okta/group.go
@@ -17,6 +17,7 @@ func listGroupUsers(ctx context.Context, m interface{}, id string) ([]*okta.User
 	for {
 		resUsers = append(resUsers, users...)
 		if resp.HasNextPage() {
+			users = []*okta.User{}
 			resp, err = resp.Next(ctx, &users)
 			if err != nil {
 				return nil, err