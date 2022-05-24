b44967828da252f3fbe3fc2e151b8b0ba462c424
Robert Mcgregor <38837341+exitcode0@users.noreply.github.com>
2022-05-24 17:21:56 +1000

    fix import okta_group_membership

--- okta/resource_okta_group_memberships.go
+++ okta/resource_okta_group_memberships.go
@@ -78,11 +78,22 @@ func resourceGroupMembershipsCreate(ctx context.Context, d *schema.ResourceData,
 
 func resourceGroupMembershipsRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	groupId := d.Get("group_id").(string)
-	users := convertInterfaceToStringSetNullable(d.Get("users"))
 	client := getOktaClientFromMetadata(m)
-	ok, err := checkIfGroupHasUsers(ctx, client, groupId, users)
+	expected_users := convertInterfaceToStringSetNullable(d.Get("users"))
 
-	logger(m).Info("reading group membership", "id", d.Id())
+	// handle import edge case
+	if len(expected_users) == 0 {
+		logger(m).Info("reading group membership", "id", d.Id())
+		userIDList, err := listGroupUserIDs(ctx, m, d.Id())
+		if err != nil {
+			return diag.Errorf("unable to return membership for group (%s) from API", d.Id())
+		}
+		d.Set("group_id", d.Id())
+		d.Set("users", convertStringSliceToSet(userIDList))
+		return nil
+	}
+
+	ok, err := checkIfGroupHasUsers(ctx, client, groupId, expected_users)
 	if err != nil {
 		return diag.Errorf("unable to complete group check for user: %v", err)
 	}
