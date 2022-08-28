6dd7be467bb4db7ab93fc5459849f7837c483b30
Tom Goodsell <ymylei@users.noreply.github.com>
2022-08-28 00:52:09 -0500

    remove scripts since paths need to be hardcoded, therefore cannot generalize

--- scripts/set-development.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-SCRIPT=$(realpath "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
-echo "Setting development override for okta/okta provider"
-export TF_CLI_CONFIG_FILE="$SCRIPTPATH/dev.tfrc"
