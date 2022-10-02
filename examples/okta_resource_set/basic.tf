59c9b5ad4ce9f558a2d78d66f77c335079105753
Mike Mondragon <mikemondragon@gmail.com>
2022-10-01 18:31:45 -0700

    Use a runtime assigned hostname variable so `TestAccOktaResourceSet` passes on all test orgs.

--- examples/okta_resource_set/basic.tf
+++ examples/okta_resource_set/basic.tf
@@ -1,9 +1,13 @@
+variable "hostname" {
+   type=string
+}
+
 resource "okta_resource_set" "test" {
   label       = "testAcc_replace_with_uuid"
   description = "testing, testing"
   resources   = [
-    "https://terraform-provider-okta.oktapreview.com/api/v1/users",
-    "https://terraform-provider-okta.oktapreview.com/api/v1/apps",
-    "https://terraform-provider-okta.oktapreview.com/api/v1/groups"
+    "https://${var.hostname}/api/v1/users",
+    "https://${var.hostname}/api/v1/apps",
+    "https://${var.hostname}/api/v1/groups"
   ]
 }
--- examples/okta_resource_set/updated.tf
+++ examples/okta_resource_set/updated.tf
@@ -1,8 +1,12 @@
+variable "hostname" {
+   type=string
+}
+
 resource "okta_resource_set" "test" {
   label       = "testAcc_replace_with_uuid"
   description = "testing, testing updated"
   resources   = [
-    "https://terraform-provider-okta.oktapreview.com/api/v1/users",
-    "https://terraform-provider-okta.oktapreview.com/api/v1/apps",
+    "https://${var.hostname}/api/v1/users",
+    "https://${var.hostname}/api/v1/apps",
   ]
 }
