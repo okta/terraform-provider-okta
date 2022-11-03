5c1c5e9e0d237eeae62a856ca9f0f9f482c1cb5d
Mike Mondragon <mikemondragon@gmail.com>
2022-11-03 15:54:17 -0700

    Making okta_link_definition resource be able to handle parallel API calls. Includes tests.

--- okta/resource_okta_link_definition_test.go
+++ okta/resource_okta_link_definition_test.go
@@ -39,3 +39,76 @@ func doesLinkDefinitionExist(id string) (bool, error) {
 	_, response, err := getOktaClientFromMetadata(testAccProvider.Meta()).LinkedObject.GetLinkedObjectDefinition(context.Background(), id)
 	return doesResourceExist(response, err)
 }
+
+// TestAccOktaUserBaseSchemaLogin_multiple_properties This test would fail
+// before the fix was implemented. The fix is to put a calling mutex on create
+// and delete for the `okta_link_definition` resource. The Okta management API
+// ignores parallel calls to `POST /api/v1/meta/schemas/user/linkedObjects` and
+// `DELETE /api/v1/meta/schemas/user/linkedObjects` and our fix is to use a
+// calling mutex in the resource to impose the equivelent of `terraform
+// -parallelism=1`
+func TestAccResourceOktaLinkDefinition_parallel_api_calls(t *testing.T) {
+	ri := acctest.RandInt()
+	mgr := newFixtureManager(linkDefinition)
+	config := `
+resource "okta_link_definition" "one" {
+	primary_name           = "testAcc_replace_with_uuid_one"
+	primary_title          = "one"
+	primary_description    = "one"
+	associated_name        = "testAcc_replace_with_uuid_one_a"
+	associated_title       = "one_a"
+	associated_description = "one_a"
+}
+resource "okta_link_definition" "two" {
+	primary_name           = "testAcc_replace_with_uuid_two"
+	primary_title          = "two"
+	primary_description    = "two"
+	associated_name        = "testAcc_replace_with_uuid_two_a"
+	associated_title       = "two_a"
+	associated_description = "two_a"
+}
+resource "okta_link_definition" "three" {
+	primary_name           = "testAcc_replace_with_uuid_three"
+	primary_title          = "three"
+	primary_description    = "three"
+	associated_name        = "testAcc_replace_with_uuid_three_a"
+	associated_title       = "three_a"
+	associated_description = "three_a"
+}
+resource "okta_link_definition" "four" {
+	primary_name           = "testAcc_replace_with_uuid_four"
+	primary_title          = "four"
+	primary_description    = "four"
+	associated_name        = "testAcc_replace_with_uuid_four_a"
+	associated_title       = "four_a"
+	associated_description = "four_a"
+}
+resource "okta_link_definition" "five" {
+	primary_name           = "testAcc_replace_with_uuid_five"
+	primary_title          = "five"
+	primary_description    = "five"
+	associated_name        = "testAcc_replace_with_uuid_five_a"
+	associated_title       = "five_a"
+	associated_description = "five_a"
+}
+`
+	config = mgr.ConfigReplace(config, ri)
+	resource.Test(t, resource.TestCase{
+		PreCheck:          testAccPreCheck(t),
+		ErrorCheck:        testAccErrorChecks(t),
+		ProviderFactories: testAccProvidersFactories,
+		CheckDestroy:      createCheckResourceDestroy(linkDefinition, doesLinkDefinitionExist),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("okta_link_definition.one", "primary_title", "one"),
+					resource.TestCheckResourceAttr("okta_link_definition.two", "primary_title", "two"),
+					resource.TestCheckResourceAttr("okta_link_definition.three", "primary_title", "three"),
+					resource.TestCheckResourceAttr("okta_link_definition.four", "primary_title", "four"),
+					resource.TestCheckResourceAttr("okta_link_definition.five", "primary_title", "five"),
+				),
+			},
+		},
+	})
+}
