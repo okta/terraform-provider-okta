6ba46ce8555815a2f64039538dc458dd38bddfa3
Mike Mondragon <mikemondragon@gmail.com>
2023-01-17 18:38:38 -0800

    Test for PR #1210 Closes #1210

--- okta/resource_okta_policy_mfa_test.go
+++ okta/resource_okta_policy_mfa_test.go
@@ -48,3 +48,58 @@ func TestAccOktaMfaPolicy_crud(t *testing.T) {
 		},
 	})
 }
+
+// TestAccOktaMfaPolicy_PR_1210 deals with testing
+// https://github.com/okta/terraform-provider-okta/pull/1210
+func TestAccOktaMfaPolicy_PR_1210(t *testing.T) {
+	ri := acctest.RandInt()
+	mgr := newFixtureManager(policyMfa)
+	config := `
+data "okta_group" "all" {
+  name = "Everyone"
+}
+resource "okta_policy_mfa" "test" {
+  name        = "testAcc_replace_with_uuid"
+  status = "ACTIVE"
+  description = "Terraform Acceptance Test MFA Policy"
+  priority = 1
+  is_oie  = true
+
+  okta_password = {
+    enroll = "REQUIRED"
+  }
+
+  okta_email = {
+    enroll = "NOT_ALLOWED"
+  }
+
+  fido_webauthn = {
+    enroll = "REQUIRED"
+  }
+
+  groups_included = [data.okta_group.all.id]
+}
+	`
+	resourceName := fmt.Sprintf("%s.test", policyMfa)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          testOIEOnlyAccPreCheck(t),
+		ErrorCheck:        testAccErrorChecks(t),
+		ProviderFactories: testAccProvidersFactories,
+		CheckDestroy:      createPolicyCheckDestroy(policyMfa),
+		Steps: []resource.TestStep{
+			{
+				Config: mgr.ConfigReplace(config, ri),
+				Check: resource.ComposeTestCheckFunc(
+					ensurePolicyExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", buildResourceName(ri)),
+					resource.TestCheckResourceAttr(resourceName, "status", statusActive),
+					resource.TestCheckResourceAttr(resourceName, "description", "Terraform Acceptance Test MFA Policy"),
+					resource.TestCheckResourceAttr(resourceName, "okta_password.enroll", "REQUIRED"),
+					resource.TestCheckResourceAttr(resourceName, "okta_email.enroll", "NOT_ALLOWED"),
+					resource.TestCheckResourceAttr(resourceName, "fido_webauthn.enroll", "REQUIRED"),
+				),
+			},
+		},
+	})
+}
