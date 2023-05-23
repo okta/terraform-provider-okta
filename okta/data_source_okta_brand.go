23db6a4ad0f719605806741c945db7114913d476
Mike Mondragon <mikemondragon@gmail.com>
2023-05-23 16:36:28 -0700

    `brand_id` can be set as `default` to retrieve the default brand w/o knowing its true ID.

--- okta/data_source_okta_brand.go
+++ okta/data_source_okta_brand.go
@@ -27,14 +27,21 @@ func dataSourceBrand() *schema.Resource {
 func dataSourceBrandRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var brand *okta.Brand
 	var err error
-	brandID, ok := d.GetOk("brand_id")
-	if ok {
-		logger(m).Info("reading brand by ID", "id", brandID.(string))
-		brand, _, err = getOktaV3ClientFromMetadata(m).CustomizationApi.GetBrand(ctx, brandID.(string)).Execute()
+	brandID := d.Get("brand_id").(string)
+
+	if brandID == "default" {
+		brand, err = getDefaultBrand(ctx, m)
+		if err != nil {
+			return diag.Errorf("failed to get default brand for org: %v", err)
+		}
+	} else {
+		logger(m).Info("reading brand by ID", "id", brandID)
+		brand, _, err = getOktaV3ClientFromMetadata(m).CustomizationApi.GetBrand(ctx, brandID).Execute()
 		if err != nil {
 			return diag.Errorf("failed to get brand: %v", err)
 		}
 	}
+
 	d.SetId(brand.GetId())
 	rawMap := flattenBrand(brand)
 	err = setNonPrimitives(d, rawMap)
