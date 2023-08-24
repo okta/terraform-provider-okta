44254660ae50d72d1fa93eb1f989dcc7cbac08aa
Tien Nguyen <duytien.nguyen@okta.com>
2023-08-24 07:41:53 -0400

    remove unused
    Signed-off-by: Tien Nguyen <duytien.nguyen@okta.com>

--- okta/brand.go
+++ okta/brand.go
@@ -9,48 +9,6 @@ import (
 	"github.com/okta/okta-sdk-golang/v3/okta"
 )
 
-var brandResourceSchema = map[string]*schema.Schema{
-	"id": {
-		Type:        schema.TypeString,
-		Computed:    true,
-		Description: "Brand ID",
-	},
-	"brand_id": {
-		Type:        schema.TypeString,
-		Optional:    true,
-		Description: "Brand ID - Note: Okta API for brands only reads and updates therefore the okta_brand resource needs to act as a quasi data source. Do this by setting brand_id.",
-	},
-	"name": {
-		Type:        schema.TypeString,
-		Computed:    true,
-		Description: "Brand name",
-	},
-	"agree_to_custom_privacy_policy": {
-		Type:         schema.TypeBool,
-		Optional:     true,
-		Description:  "Consent for updating the custom privacy policy URL.",
-		RequiredWith: []string{"custom_privacy_policy_url"},
-	},
-	"custom_privacy_policy_url": {
-		Type:             schema.TypeString,
-		Optional:         true,
-		Description:      "Custom privacy policy URL",
-		DiffSuppressFunc: suppressDuringCreateFunc("brand_id"),
-	},
-	"links": {
-		Type:        schema.TypeString,
-		Computed:    true,
-		Description: "Link relations for this object - JSON HAL - Discoverable resources related to the brand",
-	},
-	"remove_powered_by_okta": {
-		Type:             schema.TypeBool,
-		Optional:         true,
-		Default:          false,
-		Description:      `Removes "Powered by Okta" from the Okta-hosted sign-in page and "© 2021 Okta, Inc." from the Okta End-User Dashboard`,
-		DiffSuppressFunc: suppressDuringCreateFunc("brand_id"),
-	},
-}
-
 var brandDataSourceSchema = map[string]*schema.Schema{
 	"id": {
 		Type:        schema.TypeString,
