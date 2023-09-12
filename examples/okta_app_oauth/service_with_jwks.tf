fdd6f1af439f9fb78245d872466d56e11fd9d24d
Tom Goodsell <56050072+tgoodsell-tempus@users.noreply.github.com>
2023-09-11 19:05:30 -0500

    update test EC key to something valid

--- examples/okta_app_oauth/service_with_jwks.tf
+++ examples/okta_app_oauth/service_with_jwks.tf
@@ -22,8 +22,19 @@ resource "okta_app_oauth" "test2" {
 
   jwks {
     kty = "EC"
-    kid = "SIGNING_KEY"
-    x   = "AQAB"
-    y   = "owfoXNHcAlAVpIO41840ZU2tZraLGw3yEr3xZvAti7oEZPUKCytk88IDgH7440JOuz8GC_D6vtduWOqnEt0j0_faJnhKHgfj7DTWBOCxzSdjrM-Uyj6-e_XLFvZXzYsQvt52PnBJUV15G1W9QTjlghT_pFrW0xrTtbO1c281u1HJdPd5BeIyPb0pGbciySlx53OqGyxrAxPAt5P5h-n36HJkVsSQtNvgptLyOwWYkX50lgnh2szbJ0_O581bqkNBy9uqlnVeK1RZDQUl4mk8roWYhsx_JOgjpC3YyeXA6hHsT5xWZos_gNx98AHivNaAjzIzvyVItX2-hP0Aoscfff"
+    kid = "testing"
+    x   = "K37X78mXJHHldZYMzrwipjKR-YZUS2SMye0KindHp6I"
+    y   = "8IfvsvXWzbFWOZoVOMwgF5p46mUj3kbOVf9Fk0vVVHo"
   }
 }
+
+# Test EC Key
+# {
+#     "kty": "EC",
+#     "use": "sig",
+#     "crv": "P-256",
+#     "kid": "testing",
+#     "x": "K37X78mXJHHldZYMzrwipjKR-YZUS2SMye0KindHp6I",
+#     "y": "8IfvsvXWzbFWOZoVOMwgF5p46mUj3kbOVf9Fk0vVVHo",
+#     "alg": "ES256"
+# }
