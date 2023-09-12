71ef7d5ae2609cbe8201b37933cade7ab3cbe8ac
Mike Mondragon <mikemondragon@gmail.com>
2023-09-12 11:36:14 -0700

    Clean up jwks EC implementation, test, and docs

--- examples/okta_app_oauth/service_with_jwks.tf
+++ examples/okta_app_oauth/service_with_jwks.tf
@@ -7,7 +7,7 @@ resource "okta_app_oauth" "test" {
 
   jwks {
     kty = "RSA"
-    kid = "SIGNING_KEY"
+    kid = "SIGNING_KEY_RSA"
     e   = "AQAB"
     n   = "owfoXNHcAlAVpIO41840ZU2tZraLGw3yEr3xZvAti7oEZPUKCytk88IDgH7440JOuz8GC_D6vtduWOqnEt0j0_faJnhKHgfj7DTWBOCxzSdjrM-Uyj6-e_XLFvZXzYsQvt52PnBJUV15G1W9QTjlghT_pFrW0xrTtbO1c281u1HJdPd5BeIyPb0pGbciySlx53OqGyxrAxPAt5P5h-n36HJkVsSQtNvgptLyOwWYkX50lgnh2szbJ0_O581bqkNBy9uqlnVeK1RZDQUl4mk8roWYhsx_JOgjpC3YyeXA6hHsT5xWZos_gNx98AHivNaAjzIzvyVItX2-hP0Aoscfff"
   }
@@ -22,7 +22,7 @@ resource "okta_app_oauth" "test_ec" {
 
   jwks {
     kty = "EC"
-    kid = "testing"
+    kid = "SIGNING_KEY_EC"
     x   = "K37X78mXJHHldZYMzrwipjKR-YZUS2SMye0KindHp6I"
     y   = "8IfvsvXWzbFWOZoVOMwgF5p46mUj3kbOVf9Fk0vVVHo"
   }
