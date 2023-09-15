6935f6a45355befcf2f517fb0deb8ebba3af40e9
Tom Goodsell <56050072+tgoodsell-tempus@users.noreply.github.com>
2023-09-14 21:16:38 -0500

    correct test checks

--- okta/data_source_okta_org_metadata_test.go
+++ okta/data_source_okta_org_metadata_test.go
@@ -19,8 +19,8 @@ func TestAccDataSourceOktaOrgMetadata_read(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.okta_org_metadata.test", "id"),
 					resource.TestCheckResourceAttrSet("data.okta_org_metadata.test", "pipeline"),
-					resource.TestCheckResourceAttrSet("data.okta_org_metadata.test", "settings"),
-					resource.TestMatchResourceAttr("data.okta_org_metadata.test", "domains.organization", regexp.MustCompile("^.*okta.com$")),
+					resource.TestCheckResourceAttrSet("data.okta_org_metadata.test", "settings.analytics_collection_enabled"),
+					resource.TestMatchResourceAttr("data.okta_org_metadata.test", "domains.organization", regexp.MustCompile("^.*okta(?:preview)?.com$")),
 				),
 			},
 		},
