35316c5a95ce3d041519164ebcffb7c6ddbdbc21
Tom Goodsell <56050072+tgoodsell-tempus@users.noreply.github.com>
2023-09-19 15:43:46 -0500

    use oktaResourceTest, use getenv to dynamically fetch the expected org value for more compatible unit tests

--- okta/data_source_okta_org_metadata_test.go
+++ okta/data_source_okta_org_metadata_test.go
@@ -1,14 +1,15 @@
 package okta
 
 import (
-	"regexp"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccDataSourceOktaOrgMetadata_read(t *testing.T) {
-	resource.ParallelTest(t, resource.TestCase{
+	oktaResourceTest(t, resource.TestCase{
 		PreCheck:                 testAccPreCheck(t),
 		ErrorCheck:               testAccErrorChecks(t),
 		ProtoV5ProviderFactories: testAccMergeProvidersFactories,
@@ -20,7 +21,7 @@ func TestAccDataSourceOktaOrgMetadata_read(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.okta_org_metadata.test", "id"),
 					resource.TestCheckResourceAttrSet("data.okta_org_metadata.test", "pipeline"),
 					resource.TestCheckResourceAttrSet("data.okta_org_metadata.test", "settings.analytics_collection_enabled"),
-					resource.TestMatchResourceAttr("data.okta_org_metadata.test", "domains.organization", regexp.MustCompile("^.*okta(?:preview)?.com$")),
+					resource.TestCheckResourceAttr("data.okta_org_metadata.test", "domains.organization", fmt.Sprintf("https://%s", os.Getenv("TF_VAR_hostname"))),
 				),
 			},
 		},
