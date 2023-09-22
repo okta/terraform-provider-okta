a89fef8a6bea9d109615cf9ac5306d2fa987e69d
Tien Nguyen <duytien.nguyen@okta.com>
2023-09-22 15:00:22 -0400

    update file for test

--- examples/resources/okta_app_basic_auth/basic_updated.tf
+++ examples/resources/okta_app_basic_auth/basic_updated.tf
@@ -2,5 +2,5 @@ resource "okta_app_basic_auth" "test" {
   label    = "testAcc_replace_with_uuid"
   url      = "https://example.com/login.html"
   auth_url = "https://example.org/auth.html"
-  logo     = "../examples/okta_app_basic_auth/terraform_icon.png"
+  logo     = "../examples/resources/okta_app_basic_auth/terraform_icon.png"
 }
