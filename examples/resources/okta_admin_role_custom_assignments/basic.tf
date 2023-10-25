f3a78fcb056baac6add73b33691b67d1f7d46a97
Mike Mondragon <mikemondragon@gmail.com>
2023-10-25 14:31:30 -0700

    Added useful notes about the implication of setting variables with TF_VAR env vars

--- examples/resources/okta_admin_role_custom_assignments/basic.tf
+++ examples/resources/okta_admin_role_custom_assignments/basic.tf
@@ -1,3 +1,6 @@
+# For testing TF_VAR_hostname is set in provider_test.go .
+# In a live environment the operator would export `TF_VAR_hostname=[the
+# hostname]` in order to expose hostname as a variable below.
 variable "hostname" {
   type = string
 }
