e6231599969ca17d4164bcdebd196e0ee1910ef7
Mike Mondragon <mikemondragon@gmail.com>
2023-11-15 09:53:59 -0800

    Add additional test for alternate value on datasource org_metadata.

--- okta/data_source_okta_org_metadata_test.go
+++ okta/data_source_okta_org_metadata_test.go
@@ -11,6 +11,11 @@ import (
 func TestAccDataSourceOktaOrgMetadata_read(t *testing.T) {
 	mgr := newFixtureManager("data-sources", "okta_org_metadata", t.Name())
 	resourceName := fmt.Sprintf("data.%s.test", "okta_org_metadata")
+	var customDomain, customURI string
+	customDomain = os.Getenv("OKTA_ACC_TEST_CUSTOM_DOMAIN")
+	if customDomain != "" {
+		customURI = fmt.Sprintf("https://%s", customDomain)
+	}
 
 	oktaResourceTest(t, resource.TestCase{
 		PreCheck:                 testAccPreCheck(t),
@@ -24,6 +29,7 @@ func TestAccDataSourceOktaOrgMetadata_read(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "pipeline"),
 					resource.TestCheckResourceAttrSet(resourceName, "settings.analytics_collection_enabled"),
 					resource.TestCheckResourceAttr(resourceName, "domains.organization", fmt.Sprintf("https://%s.%s", os.Getenv("OKTA_ORG_NAME"), os.Getenv("OKTA_BASE_URL"))),
+					resource.TestCheckResourceAttr(resourceName, "domains.alternate", customURI),
 				),
 			},
 		},
