7ae224f23b3488f831ccfc689ef4f016c5b0b61d
Rob McGregor <38837341+exitcode0@users.noreply.github.com>
2025-01-16 16:32:42 +1100

    suppress 404 when reading admin roles to fix deadlock
    If user targeted by `okta_user_admin_roles` was deleted, the resource
    could not be removed without CLI intervention as the 404 error will
    cause Terraform plan to fail
    
    by suppressing the 404 here the user can successfully run terraform apply
    to remove the resource from both the state file and configuration
    
    unfortunately as a result we no longer guard against 404 errors and
    users may attempt to re-create invalid admin role assignments, however
    in my opinion the pros outweight the cons here as the user
    does not need to venture outside version control to restore
    deployability

--- okta/resource_okta_user_admin_roles.go
+++ okta/resource_okta_user_admin_roles.go
@@ -59,10 +59,18 @@ func resourceUserAdminRolesCreate(ctx context.Context, d *schema.ResourceData, m
 }
 
 func resourceUserAdminRolesRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	err := setAdminRoles(ctx, d, m)
+	adminRoles, resp, err := getAdminRoles(ctx, d.Id(), getOktaClientFromMetadata(m))
+
 	if err != nil {
-		return diag.Errorf("failed to set read user's roles: %v", err)
+		if err := suppressErrorOn404(resp, err); err == nil {
+			// The targetd_user or their roles have been deleted since our last Terraform run.
+			d.SetId("")
+			return nil
+		}
+		return diag.Errorf("failed to get user admin roles: %v", err)
 	}
+
+	_ = d.Set("admin_roles", schema.NewSet(schema.HashString, adminRoles))
 	return nil
 }
 
