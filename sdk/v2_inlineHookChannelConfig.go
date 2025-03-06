456d8fbcfd46df04b7bd3e3ab9b3b838c3fc1987
Mike Mondragon <mikemondragon@gmail.com>
2025-03-06 12:10:58 -0800

    Expose full channel API contract in `okta_inline_hook` resource as `channel_json` so that OAuth auth values and header values can be set when the inline hook is managed.
    Closes #2190

--- sdk/v2_inlineHookChannelConfig.go
+++ sdk/v2_inlineHookChannelConfig.go
@@ -2,8 +2,13 @@
 package sdk
 
 type InlineHookChannelConfig struct {
-	AuthScheme *InlineHookChannelConfigAuthScheme `json:"authScheme,omitempty"`
-	Headers    []*InlineHookChannelConfigHeaders  `json:"headers,omitempty"`
-	Method     string                             `json:"method,omitempty"`
-	Uri        string                             `json:"uri,omitempty"`
+	AuthScheme   *InlineHookChannelConfigAuthScheme `json:"authScheme,omitempty"`
+	Headers      []*InlineHookChannelConfigHeaders  `json:"headers,omitempty"`
+	Method       string                             `json:"method,omitempty"`
+	Uri          string                             `json:"uri,omitempty"`
+	AuthType     string                             `json:"authType,omitempty"`
+	ClientId     string                             `json:"clientId,omitempty"`
+	ClientSecret string                             `json:"clientSecret,omitempty"`
+	TokenUrl     string                             `json:"tokenUrl,omitempty"`
+	Scope        string                             `json:"scope,omitempty"`
 }
