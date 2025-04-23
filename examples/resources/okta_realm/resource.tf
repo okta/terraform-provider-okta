aaaf13c86ed1ef077e7e67aaf481cb7374bbc84a
Tim Koehler <mail@koehlertim.de>
2025-04-23 18:21:06 +0200

    feat: adds basic realm support

---
+++ examples/resources/okta_realm/resource.tf
@@ -0,0 +1,4 @@
+resource "okta_realm" "example" {
+  name       = "Example Realm"
+  realm_type = "DEFAULT"
+}
--- okta/framework_provider.go
+++ okta/framework_provider.go
@@ -256,6 +256,7 @@ func (p *FrameworkProvider) DataSources(_ context.Context) []func() datasource.D
 		NewAppsDataSource,
 		NewUserTypeDataSource,
 		NewDeviceAssurancePolicyDataSource,
+		NewRealmDataSource,
 	}
 }
 
@@ -277,6 +278,7 @@ func (p *FrameworkProvider) Resources(_ context.Context) []func() resource.Resou
 		NewGroupOwnerResource,
 		NewAppSignOnPolicyResource,
 		NewEmailTemplateSettingsResource,
+		NewRealmResource,
 	}
 }
 
