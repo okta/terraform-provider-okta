31978041fc506318b8077f0ddd4aeac5dce657a1
Tien Nguyen <duytien.nguyen@okta.com>
2025-04-25 14:31:00 -0400

    add support for honorPersistentNameID for okta_idp_saml
    Signed-off-by: Tien Nguyen <duytien.nguyen@okta.com>

--- sdk/v2_protocolSettings.go
+++ sdk/v2_protocolSettings.go
@@ -1,6 +1,6 @@
-// DO NOT EDIT LOCAL SDK - USE v3 okta-sdk-golang FOR API CALLS THAT DO NOT EXIST IN LOCAL SDK
 package sdk
 
 type ProtocolSettings struct {
-	NameFormat string `json:"nameFormat,omitempty"`
+	NameFormat            string `json:"nameFormat,omitempty"`
+	HonorPersistentNameId bool   `json:"honorPersistentNameId"`
 }
