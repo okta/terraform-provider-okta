680b32d405a8bfe4938c363bc3de6de2ef557b4b
aditya-okta <aditya.anand@okta.com>
2025-06-01 01:55:30 +0530

    updated indentation

--- okta/services/idaas/resource_okta_app_user_custom_schema_property_test.go
+++ okta/services/idaas/resource_okta_app_user_custom_schema_property_test.go
@@ -381,34 +381,34 @@ func TestAccResourceOktaAppUserSchemas_enum_boolean(t *testing.T) {
 	mgr := newFixtureManager("resources", resources.OktaIDaaSAppUserSchemaProperty, t.Name())
 	resourceName := fmt.Sprintf("%s.test", resources.OktaIDaaSAppUserSchemaProperty)
 	config := `
-	resource "okta_app_oauth" "test" {
-		label          = "testAcc_replace_with_uuid"
-		type           = "native"
-		grant_types    = ["authorization_code"]
-		redirect_uris  = ["http://d.com/"]
-		response_types = ["code"]
-		}
-	
-	resource "okta_app_user_schema_property" "test" {
-		app_id      = okta_app_oauth.test.id
-		index       = "testAcc_replace_with_uuid"
-		title       = "terraform acceptance test"
-		type        = "string"
-		description = "testing"
-		required    = false
-		permissions = "READ_ONLY"
-		master      = "PROFILE_MASTER"
-		enum  		= ["true", "false"]
-		one_of {
-	  		title = "boolean True"
-	  		const = "true"
-		}
-		one_of {
-	  		title = "boolean False"
-	  		const = "false"
-		}
+resource "okta_app_oauth" "test" {
+	label          = "testAcc_replace_with_uuid"
+	type           = "native"
+	grant_types    = ["authorization_code"]
+	redirect_uris  = ["http://d.com/"]
+	response_types = ["code"]
 	}
-	`
+	
+resource "okta_app_user_schema_property" "test" {
+	app_id      = okta_app_oauth.test.id
+	index       = "testAcc_replace_with_uuid"
+	title       = "terraform acceptance test"
+	type        = "string"
+	description = "testing"
+	required    = false
+	permissions = "READ_ONLY"
+	master      = "PROFILE_MASTER"
+	enum  		= ["true", "false"]
+	one_of {
+		title = "boolean True"
+		const = "true"
+	}
+	one_of {
+		title = "boolean False"
+		const = "false"
+	}
+}
+`
 	acctest.OktaResourceTest(t, resource.TestCase{
 		PreCheck:                 acctest.AccPreCheck(t),
 		ErrorCheck:               testAccErrorChecks(t),
