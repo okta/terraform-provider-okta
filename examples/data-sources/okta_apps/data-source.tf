9947e691dbe241fa46ef4de28f6fa309eca0401e
Pranav RK <pranav.rathinakumar@okta.com>
2025-06-18 12:56:22 +0530

    chore: fix indentation

--- examples/data-sources/okta_apps/data-source.tf
+++ examples/data-sources/okta_apps/data-source.tf
@@ -1,5 +1,5 @@
 data "okta_apps" "example" {
-q                   = "Example"
-active_only         = true
-include_non_deleted = true
+  q                   = "Example"
+  active_only         = true
+  include_non_deleted = true
 }
