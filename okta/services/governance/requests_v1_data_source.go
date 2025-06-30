ea398bf56cf3ee6ee4c15028d71e05f2ddfb1375
Mike Mondragon <mikemondragon@gmail.com>
2025-06-30 16:08:24 -0700

    OIG resources and datasources codegen'd with tfplugingen-framework
    ```
    for name in `cat ~/tmp/oig/resources.txt`; do
      tfplugingen-framework scaffold resource \
        --name $name \
        --force \
        --output-dir okta/services/governance
    done
    
    for name in `cat ~/tmp/oig/datasources.txt`; do
      tfplugingen-framework scaffold data-source \
        --name $name \
        --force \
        --output-dir okta/services/governance
    done
    ```

---
+++ okta/services/governance/requests_v1_data_source.go
@@ -0,0 +1,54 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var _ datasource.DataSource = (*requestsV1DataSource)(nil)
+
+func NewRequestsV1DataSource() datasource.DataSource {
+	return &requestsV1DataSource{}
+}
+
+type requestsV1DataSource struct{}
+
+type requestsV1DataSourceModel struct {
+	Id types.String `tfsdk:"id"`
+}
+
+func (d *requestsV1DataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_requests_v1"
+}
+
+func (d *requestsV1DataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+			},
+		},
+	}
+}
+
+func (d *requestsV1DataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data requestsV1DataSourceModel
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Read API call logic
+
+	// Example data value setting
+	data.Id = types.StringValue("example-id")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
