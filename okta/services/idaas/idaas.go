f22d9ab1e9a4437ea64f2f29aaa9702d1eed5fb2
Rob McGregor <38837341+exitcode0@users.noreply.github.com>
2025-07-07 17:59:55 +1000

    feat: ephemeral value support in okta_event_hook
    Migrate okta_event_hook resource from SDKv2 to Plugin Framework
    to facilitate ephemeral value support with the auth.value attribute
    
    Fixes #2385

--- okta/services/idaas/idaas.go
+++ okta/services/idaas/idaas.go
@@ -124,6 +124,7 @@ func FWProviderResources() []func() resource.Resource {
 		newPushGroupResource,
 		newUserRiskResource,
 		newPostAuthSessionPolicyRuleResource,
+		newEventHookResource,
 	}
 	// Wrap all resources with SafeResource for panic recovery
 	return resources.WrapResources(rawResources)
@@ -210,7 +211,6 @@ func ProviderResources() map[string]*schema.Resource {
 		resources.OktaIDaaSEmailSender:                   resourceEmailSender(),
 		resources.OktaIDaaSEmailSenderVerification:       resourceEmailSenderVerification(),
 		resources.OktaIDaaSEmailSMTPServer:               resourceEmailSMTP(),
-		resources.OktaIDaaSEventHook:                     resourceEventHook(),
 		resources.OktaIDaaSEventHookVerification:         resourceEventHookVerification(),
 		resources.OktaIDaaSFactor:                        resourceFactor(),
 		resources.OktaIDaaSFactorTotp:                    resourceFactorTOTP(),
