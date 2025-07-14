8aede75a43901ffa73edfbf2728ada1c469911d6
Rob McGregor <38837341+exitcode0@users.noreply.github.com>
2025-07-15 08:37:11 +1000

    feat: add resource-set datasources + examples
    - Add okta_resource_sets and okta_resource_set_resources data sources
    - Update okta_resource_set data source and resource for consistency
    - Add and update example configurations and infrastructure

---
+++ examples/data-sources/okta_resource_sets/data-source.tf
@@ -0,0 +1,25 @@
+# Example configuration showing how to use the okta_resource_sets data source
+# This example demonstrates how to retrieve all resource sets in the organization
+
+# Get all resource sets
+data "okta_resource_sets" "all" {
+}
+
+# Example output showing the resource sets
+output "resource_sets_count" {
+  description = "The number of resource sets found"
+  value       = length(data.okta_resource_sets.all.resource_sets)
+}
+
+output "resource_sets" {
+  description = "List of all resource sets"
+  value       = data.okta_resource_sets.all.resource_sets
+}
+
+# Example of filtering resource sets by label (if needed in the future)
+# This would require adding a filter parameter to the datasource
+/*
+data "okta_resource_sets" "filtered" {
+  label_prefix = "prod-"
+}
+*/ 
