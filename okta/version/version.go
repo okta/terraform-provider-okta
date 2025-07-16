3ff45a88cd7968a8a3fabf1922b6172e645623d0
aditya-okta <aditya.anand@okta.com>
2025-07-16 15:17:11 +0530

    updates version

--- okta/version/version.go
+++ okta/version/version.go
@@ -1,6 +1,6 @@
 package version
 
 var (
-	OktaTerraformProviderVersion   = "5.1.0"
+	OktaTerraformProviderVersion   = "5.2.0"
 	OktaTerraformProviderUserAgent = "okta-terraform/" + OktaTerraformProviderVersion
 )
