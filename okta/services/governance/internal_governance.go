4eb648d0de18c5bdda48b95d8cde5f61028c6334
Dhiwakar Ravikumar <dhiwakar.ravikumar@okta.com>
2025-08-01 17:55:32 +0530

    OKTA-986936 | Adding Terraform Resource For End User My Requests

--- okta/services/governance/internal_governance.go
+++ okta/services/governance/internal_governance.go
@@ -2,8 +2,15 @@ package governance
 
 import (
 	"fmt"
+
+	"github.com/okta/terraform-provider-okta/okta/config"
+
+	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
 	"github.com/okta/terraform-provider-okta/okta/config"
 )
 
@@ -16,7 +23,7 @@ func FWProviderResources() []func() resource.Resource {
 		newGrantResource,
 		newRiskRuleResource,
 		newCollectionResource,
-		newRequestTypeResource,
+		newMyRequestsResource,
 	}
 }
 
@@ -32,6 +39,55 @@ func FWProviderDataSources() []func() datasource.DataSource {
 		newRiskRulesDataSource,
 		newCollectionDataSource,
 	}
+}
+
+func ProviderResources() map[string]*schema.Resource {
+	return map[string]*schema.Resource{}
+}
+
+func ProviderDataSources() map[string]*schema.Resource {
+	return map[string]*schema.Resource{
+
+		// resources.OktaInternalGovernanceCampaigns:    dataSourceCampaigns(),
+	}
+}
+
+func stringIsJSON(i interface{}, k cty.Path) diag.Diagnostics {
+	v, ok := i.(string)
+	if !ok {
+		return diag.Errorf("expected type of %s to be string", k)
+	}
+	if v == "" {
+		return diag.Errorf("expected %q JSON to not be empty, got %v", k, i)
+	}
+	if _, err := structure.NormalizeJsonString(v); err != nil {
+		return diag.Errorf("%q contains an invalid JSON: %s", k, err)
+	}
+	return nil
+}
+
+// doNotRetry helper function to flag if provider should be using backoff.Retry
+func doNotRetry(m interface{}, err error) bool {
+	return m.(*config.Config).TimeOperations.DoNotRetry(err)
+}
+
+func datasourceOIEOnlyFeatureError(name string) diag.Diagnostics {
+	return oieOnlyFeatureError("data-sources", name)
+}
+
+func oieOnlyFeatureError(kind, name string) diag.Diagnostics {
+	url := fmt.Sprintf("https://registry.terraform.io/providers/okta/okta/latest/docs/%s/%s", kind, string(name[5:]))
+	if kind == "resources" {
+		kind = "resource"
+	}
+	if kind == "data-sources" {
+		kind = "datasource"
+	}
+	return diag.Errorf("%q is a %s for OIE Orgs only, see %s", name, kind, url)
+}
+
+func resourceOIEOnlyFeatureError(name string) diag.Diagnostics {
+	return oieOnlyFeatureError("resources", name)
 }
 
 func dataSourceConfiguration(req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) *config.Config {
