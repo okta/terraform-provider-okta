3b5144a662f45a6b7cfbbcff2ede9203159b613f
aditya-okta <aditya.anand@okta.com>
2025-08-04 20:42:19 +0530

    adds vcr cassettes for entitlement and campaigns

---
+++ okta/services/governance/resource_entitlement_test.go
@@ -0,0 +1,33 @@
+package governance_test
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/okta/terraform-provider-okta/okta/acctest"
+	"github.com/okta/terraform-provider-okta/okta/resources"
+	"testing"
+)
+
+func TestAccEntitlement_basic(t *testing.T) {
+	mgr := newFixtureManager("resources", resources.OktaInternalGovernanceEntitlement, t.Name())
+	config := mgr.GetFixtures("basic.tf", t)
+	resourceName := fmt.Sprintf("%s.test", resources.OktaInternalGovernanceEntitlement)
+	acctest.OktaResourceTest(t, resource.TestCase{
+		PreCheck:                 acctest.AccPreCheck(t),
+		ErrorCheck:               testAccErrorChecks(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactoriesForTestAcc(t),
+		CheckDestroy:             nil,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "Entitlement Bundle"),
+					resource.TestCheckResourceAttr(resourceName, "external_value", "Entitlement Bundle"),
+					resource.TestCheckResourceAttr(resourceName, "parent.type", "APPLICATION"),
+					resource.TestCheckResourceAttr(resourceName, "values.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "values.0.external_value", "value_1"),
+				),
+			},
+		},
+	})
+}
