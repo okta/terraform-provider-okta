f50804d801834e9edc34a2befbb3dd0f4353ce78
dhiwakar-okta <dhiwakar.ravikumar@okta.com>
2025-08-05 06:38:48 +0530

    OKTA-817126 | Adding Missing Fields To Terraform State File To Avoid False Diffs For Token Type Inline Hooks (#2420)

---
+++ examples/resources/okta_inline_hook/okta_inline_hook_token_channel_auth_updated.tf
@@ -0,0 +1,18 @@
+resource "okta_inline_hook" "token_channel_auth" {
+  name    = "Inline Hook Channel Auth"
+  version = "1.0.0"
+  type    = "com.okta.oauth2.tokens.transform"
+
+  channel = {
+    version = "1.0.0"
+    uri     = "https://example.com/test"
+    method  = "POST"
+  }
+
+  auth = {
+    key   = "Authorization"
+    type  = "HEADER"
+    value = "secret_UPDATED"
+  }
+}
+
