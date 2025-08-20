66c1cd3d89012fcf629d5f298ccb1665d8fd122c
dhiwakar-okta <dhiwakar.ravikumar@okta.com>
2025-08-20 14:13:55 +0530

    GH Issue # 2419 | Attribute risk_level In okta_policy_rule_signon Is Not Honored  (#2436)
    * OKTA-995128 | Attribute risk_level In okta_policy_rule_signon Not Honored Correctly
    
    * OKTA-995128 | Changes After Testing
    
    * OKTA-995128 | Adding the test files

--- examples/resources/okta_policy_rule_signon/basic_updated.tf
+++ examples/resources/okta_policy_rule_signon/basic_updated.tf
@@ -16,6 +16,13 @@ resource "okta_policy_signon" "test" {
   groups_included = [data.okta_group.all.id]
 }
 
+resource "okta_policy_signon" "test_two" {
+  name            = "test_two"
+  status          = "ACTIVE"
+  description     = "Terraform Acceptance Test SignOn Policy"
+  groups_included = [data.okta_group.all.id]
+}
+
 resource "okta_policy_rule_signon" "test" {
   policy_id          = okta_policy_signon.test.id
   name               = "testAcc_replace_with_uuid"
@@ -25,4 +32,26 @@ resource "okta_policy_rule_signon" "test" {
   session_lifetime   = 240
   session_persistent = false
   users_excluded     = [okta_user.test.id]
+}
+
+resource "okta_policy_rule_signon" "test_risk_ONLY" {
+  policy_id       = okta_policy_signon.test_two.id
+  name            = "test_policy_risk_ONLY"
+  status          = "ACTIVE"
+  risk_level      = "MEDIUM"
+}
+
+resource "okta_policy_rule_signon" "test_risc_ONLY" {
+  policy_id       = okta_policy_signon.test_two.id
+  name            = "test_policy_risc_ONLY"
+  status          = "ACTIVE"
+  risc_level      = "HIGH"
+} 
+
+resource "okta_policy_rule_signon" "test_BOTH" {
+  policy_id       = okta_policy_signon.test_two.id
+  name            = "test_policy_BOTH"
+  status          = "ACTIVE"
+  risk_level      = "MEDIUM"
+  risc_level      = "HIGH"
 }
