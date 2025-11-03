da7bdb599bf6e791313dbe44ef29be3dc64ccbaa
Dhiwakar Ravikumar <dhiwakar.ravikumar@okta.com>
2025-11-03 17:58:02 +0530

    Reapply "Update golangci yml (#2545)"
    This reverts commit 8dc5fe88b0511315889b502a270f353b9c62b007.

--- examples/resources/okta_principal_rate_limits/updated.tf
+++ examples/resources/okta_principal_rate_limits/updated.tf
@@ -1,6 +1,6 @@
-resource "okta_principal_rate_limits" test{
-  principal_id = "00T1cuuokxSImd8ST1d7"
-  principal_type = "SSWS_TOKEN"
-  default_percentage = 55
-  default_concurrency_percentage =85
+resource "okta_principal_rate_limits" "test" {
+  principal_id                   = "00T1cuuokxSImd8ST1d7"
+  principal_type                 = "SSWS_TOKEN"
+  default_percentage             = 55
+  default_concurrency_percentage = 85
 }
--- examples/resources/okta_rate_limiting/basic.tf
+++ examples/resources/okta_rate_limiting/basic.tf
@@ -1,6 +1,6 @@
 resource "okta_rate_limiting" "example" {
   default_mode = "ENFORCE"
-  use_case_mode_overrides{
-    login_page= "ENFORCE"
+  use_case_mode_overrides {
+    login_page = "ENFORCE"
   }
 }
