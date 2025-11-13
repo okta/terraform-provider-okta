43b16c7a8c49d675a5c7147a1f5636773351d90e
Rob McGregor <38837341+exitcode0@users.noreply.github.com>
2025-11-13 17:06:18 +1100

    feat(governance): add status management for request conditions
    Add support for activating and deactivating request conditions through
    the status field. When status is ACTIVE or INACTIVE, the resource calls
    ActivateResourceRequestConditionV2 or DeactivateResourceRequestConditionV2.
    
    Fixes #2525

--- examples/resources/okta_request_condition/basic.tf
+++ examples/resources/okta_request_condition/basic.tf
@@ -9,3 +9,16 @@ resource "okta_request_condition" "test" {
     type = "EVERYONE"
   }
 }
+
+resource "okta_request_condition" "test_active" {
+  status               = "ACTIVE"
+  resource_id          = "0oaoum6j3cElINe1z1d7"
+  approval_sequence_id = "68920b41386747a673869356"
+  name                 = "test-condition-active"
+  access_scope_settings {
+    type = "RESOURCE_DEFAULT"
+  }
+  requester_settings {
+    type = "EVERYONE"
+  }
+}
