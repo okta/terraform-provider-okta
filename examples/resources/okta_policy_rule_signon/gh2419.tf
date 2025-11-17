cd4eee22f560e1cf23ae7c5905acef2de25f3280
Oliver Hookins <ohookins@gmail.com>
2025-11-17 14:23:06 +1100

    Revert original test fixtures and create unique ones for this case.

--- examples/resources/okta_policy_rule_signon/gh2419.tf
+++ examples/resources/okta_policy_rule_signon/gh2419.tf
@@ -37,9 +37,3 @@ resource "okta_policy_rule_signon" "test_BOTH" {
   risk_level = "LOW"
   risc_level = "HIGH"
 }
-
-resource "okta_policy_rule_signon" "test_NEITHER" {
-  policy_id = "00ppe8c4f0jC1KpxX1d7"
-  name      = "test_policy_NEITHER"
-  status    = "ACTIVE"
-}
