cf2445574da172733152b9b290580519fed14ec7
dhiwakar-okta <dhiwakar.ravikumar@okta.com>
2025-11-25 22:01:30 +0530

    prepare release v6.5.1 (#2576)

--- okta/version/version.go
+++ okta/version/version.go
@@ -1,6 +1,6 @@
 package version
 
 var (
-	OktaTerraformProviderVersion   = "6.5.0"
+	OktaTerraformProviderVersion   = "6.5.1"
 	OktaTerraformProviderUserAgent = "okta-terraform/" + OktaTerraformProviderVersion
 )
