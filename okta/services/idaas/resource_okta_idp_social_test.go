50b5fdc31ed0ae658fd6d0a3c6f5a8f17e3769d9
Dogacan Colak <dogacancolak@gmail.com>
2026-03-02 09:49:17 -0500

    Add write-only client_secret_wo to okta_idp_social

--- okta/services/idaas/resource_okta_idp_social_test.go
+++ okta/services/idaas/resource_okta_idp_social_test.go
@@ -65,3 +65,48 @@ func TestAccResourceOktaIdpSocial_crud(t *testing.T) {
 		},
 	})
 }
+
+// TestAccResourceOktaIdpSocial_clientSecretWo tests the write-only client_secret_wo attribute
+func TestAccResourceOktaIdpSocial_clientSecretWo(t *testing.T) {
+	config := `
+resource "okta_idp_social" "test" {
+  type          = "FACEBOOK"
+  protocol_type = "OAUTH2"
+  name          = "testAcc_replace_with_uuid"
+
+  scopes = [
+    "public_profile",
+    "email",
+  ]
+
+  client_id         = "abcd123"
+  client_secret_wo  = "secret_from_writeonly_attr"
+  username_template = "idpuser.email"
+}`
+
+	mgr := newFixtureManager("resources", resources.OktaIDaaSIdpSocial, t.Name())
+	resourceName := fmt.Sprintf("%s.test", resources.OktaIDaaSIdpSocial)
+
+	acctest.OktaResourceTest(t, resource.TestCase{
+		PreCheck:                 acctest.AccPreCheck(t),
+		ErrorCheck:               testAccErrorChecks(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactoriesForTestAcc(t),
+		CheckDestroy:             checkResourceDestroy(resources.OktaIDaaSIdpSocial, createDoesIdpExist),
+		Steps: []resource.TestStep{
+			{
+				Config: mgr.ConfigReplace(config),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "type", "FACEBOOK"),
+					resource.TestCheckResourceAttr(resourceName, "protocol_type", "OAUTH2"),
+					resource.TestCheckResourceAttr(resourceName, "name", acctest.BuildResourceName(mgr.Seed)),
+					resource.TestCheckResourceAttr(resourceName, "client_id", "abcd123"),
+					resource.TestCheckResourceAttr(resourceName, "username_template", "idpuser.email"),
+					// client_secret_wo should not be persisted in state
+					resource.TestCheckNoResourceAttr(resourceName, "client_secret_wo"),
+					// Verify client_secret is not set when using write-only attribute
+					resource.TestCheckResourceAttr(resourceName, "client_secret", ""),
+				),
+			},
+		},
+	})
+}
