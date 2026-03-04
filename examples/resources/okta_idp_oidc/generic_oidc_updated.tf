0d34451712ee21f085efdde1816561f3d56135d1
dhiwakar-okta <dhiwakar.ravikumar@okta.com>
2026-03-04 12:10:32 +0530

    Adding Support For "Enable Trust Claims For IdP" For okta_idp_saml & okta_idp_oidc (#2698)

--- examples/resources/okta_idp_oidc/generic_oidc_updated.tf
+++ examples/resources/okta_idp_oidc/generic_oidc_updated.tf
@@ -14,4 +14,5 @@ resource "okta_idp_oidc" "test" {
   issuer_url            = "https://id.example.com"
   username_template     = "idpuser.email"
   filter                = "xyz"
+  trust_claims          = false
 }
