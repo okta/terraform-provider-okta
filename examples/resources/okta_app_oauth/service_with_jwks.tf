71e01eb112123255d95c64397badf6764d41d747
Pranav RK <pranav.rathinakumar@okta.com>
2026-03-25 21:30:36 +0530

    fix: update VCR cassettes for okta_app_oauth

--- examples/resources/okta_app_oauth/service_with_jwks.tf
+++ examples/resources/okta_app_oauth/service_with_jwks.tf
@@ -5,12 +5,7 @@ resource "okta_app_oauth" "test" {
   grant_types                = ["client_credentials"]
   token_endpoint_auth_method = "private_key_jwt"
 
-  jwks {
-    kty = "RSA"
-    kid = "SIGNING_KEY_RSA"
-    e   = "AQAB"
-    n   = "owfoXNHcAlAVpIO41840ZU2tZraLGw3yEr3xZvAti7oEZPUKCytk88IDgH7440JOuz8GC_D6vtduWOqnEt0j0_faJnhKHgfj7DTWBOCxzSdjrM-Uyj6-e_XLFvZXzYsQvt52PnBJUV15G1W9QTjlghT_pFrW0xrTtbO1c281u1HJdPd5BeIyPb0pGbciySlx53OqGyxrAxPAt5P5h-n36HJkVsSQtNvgptLyOwWYkX50lgnh2szbJ0_O581bqkNBy9uqlnVeK1RZDQUl4mk8roWYhsx_JOgjpC3YyeXA6hHsT5xWZos_gNx98AHivNaAjzIzvyVItX2-hP0Aoscfff"
-  }
+  jwks_uri = "https://example.com/.well-known/jwks.json"
 }
 
 resource "okta_app_oauth" "test_ec" {
@@ -20,12 +15,7 @@ resource "okta_app_oauth" "test_ec" {
   grant_types                = ["client_credentials"]
   token_endpoint_auth_method = "private_key_jwt"
 
-  jwks {
-    kty = "EC"
-    kid = "SIGNING_KEY_EC"
-    x   = "K37X78mXJHHldZYMzrwipjKR-YZUS2SMye0KindHp6I"
-    y   = "8IfvsvXWzbFWOZoVOMwgF5p46mUj3kbOVf9Fk0vVVHo"
-  }
+  jwks_uri = "https://example.com/.well-known/jwks-ec.json"
 }
 
 # Test EC Key
