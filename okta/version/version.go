53d32a73af496621da4d737ff982ec6a15f02261
Aditya Anand <aditya.anand@okta.com>
2026-03-27 18:04:51 +0530

    prepare release v6.7.0 (#2756)

--- okta/version/version.go
+++ okta/version/version.go
@@ -1,6 +1,6 @@
 package version
 
 var (
-	OktaTerraformProviderVersion   = "6.6.1"
+	OktaTerraformProviderVersion   = "6.7.0"
 	OktaTerraformProviderUserAgent = "okta-terraform/" + OktaTerraformProviderVersion
 )
