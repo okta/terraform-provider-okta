3813e525f67ea19a0224b261471ba6bbea42efb1
Pranav RK <pranav.rathinakumar@okta.com>
2026-04-09 10:17:09 +0530

    feat: add authenticator methods and webauth custom aaguids

---
+++ examples/resources/okta_authenticator_webauthn_custom_aaguid/okta_authenticator_webauthn_custom_aaguid.tf
@@ -0,0 +1,15 @@
+data "okta_authenticator" "webauthn" {
+  key = "webauthn"
+}
+
+resource "okta_authenticator_webauthn_custom_aaguid" "sample" {
+  authenticator_id = data.okta_authenticator.webauthn.id
+  aaguid           = "cb69481e-8ff7-4039-93ec-0a2729a154a8"
+  name             = "YubiKey 5 Series"
+
+  authenticator_characteristics {
+    fips_compliant     = true
+    hardware_protected = true
+    platform_attached  = false
+  }
+}
