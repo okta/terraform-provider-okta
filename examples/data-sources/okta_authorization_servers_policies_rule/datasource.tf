feea5e786087fa99a2e3891544f89b7355ea59d7
aditya-okta <aditya.anand@okta.com>
2026-05-31 15:20:36 +0530

    adds data source for auth server policy rule

---
+++ examples/data-sources/okta_authorization_servers_policies_rule/datasource.tf
@@ -0,0 +1,33 @@
+data "okta_group" "all" {
+  name = "Everyone"
+}
+
+resource "okta_auth_server" "test" {
+  name        = "testAcc_replace_with_uuid"
+  description = "test"
+  audiences   = ["whatever.rise.zone"]
+}
+
+resource "okta_auth_server_policy" "test" {
+  name             = "test"
+  description      = "test"
+  priority         = 1
+  client_whitelist = ["ALL_CLIENTS"]
+  auth_server_id   = okta_auth_server.test.id
+}
+
+resource "okta_auth_server_policy_rule" "test" {
+  auth_server_id       = okta_auth_server.test.id
+  policy_id            = okta_auth_server_policy.test.id
+  status               = "ACTIVE"
+  name                 = "test"
+  priority             = 1
+  group_whitelist      = [data.okta_group.all.id]
+  grant_type_whitelist = ["implicit"]
+}
+
+data "okta_authorization_servers_policies_rule" "test" {
+  id             = okta_auth_server_policy_rule.test.id
+  auth_server_id = okta_auth_server.test.id
+  policy_id      = okta_auth_server_policy.test.id
+}
