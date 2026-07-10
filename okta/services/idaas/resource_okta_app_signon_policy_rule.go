09907a6b6c69972ffb5d217e06376b5ce0942a33
aditya-okta <aditya.anand@okta.com>
2026-07-10 19:13:46 +0530

    fix: office365_client_include perpetual diff and add validation

--- okta/services/idaas/resource_okta_app_signon_policy_rule.go
+++ okta/services/idaas/resource_okta_app_signon_policy_rule.go
@@ -265,7 +265,10 @@ The only difference is that these fields are immutable and can not be managed: '
 				Type:        schema.TypeSet,
 				Optional:    true,
 				Description: "Office 365 client types to include. Valid values: WEB, MODERN_AUTH, EXCHANGE_ACTIVE_SYNC, AAD_JOIN. This condition is specific to Office 365 applications.",
-				Elem:        &schema.Schema{Type: schema.TypeString},
+				Elem: &schema.Schema{
+					Type:             schema.TypeString,
+					ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice([]string{"WEB", "MODERN_AUTH", "EXCHANGE_ACTIVE_SYNC", "AAD_JOIN"}, false)),
+				},
 			},
 		},
 	}
@@ -395,10 +398,14 @@ func resourceAppSignOnPolicyRuleRead(ctx context.Context, d *schema.ResourceData
 		if rule.Conditions.RiskScore != nil {
 			_ = d.Set("risk_score", rule.Conditions.RiskScore.Level)
 		}
-		if rule.Conditions.Office365Client != nil && len(rule.Conditions.Office365Client.Include) > 0 {
-			m["office365_client_include"] = utils.ConvertStringSliceToSetNullable(rule.Conditions.Office365Client.Include)
-		}
 		_ = utils.SetNonPrimitives(d, m)
+		var office365Includes []interface{}
+		if rule.Conditions.Office365Client != nil {
+			for _, v := range rule.Conditions.Office365Client.Include {
+				office365Includes = append(office365Includes, v)
+			}
+		}
+		_ = d.Set("office365_client_include", schema.NewSet(schema.HashString, office365Includes))
 	}
 	return nil
 }
