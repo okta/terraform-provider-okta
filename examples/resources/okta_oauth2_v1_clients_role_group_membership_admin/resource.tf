8afa5444d30b0ff7f684ab2eb501c1e83464a1c0
Pranav RK <pranav.rathinakumar@okta.com>
2026-07-30 14:49:32 +0530

    feat: add new resources for oauthv1 clients (#2886)

---
+++ examples/resources/okta_oauth2_v1_clients_role_group_membership_admin/resource.tf
@@ -0,0 +1,4 @@
+resource "okta_oauth2_v1_clients_role_group_membership_admin" "example" {
+  client_id = "<client_id>"
+  type      = "GROUP_MEMBERSHIP_ADMIN"
+}
---
+++ okta/services/idaas/resource_okta_oauth2_v1_clients_role_help_desk_admin_test.go
@@ -0,0 +1,61 @@
+// Copyright 2025 - Present Okta, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package idaas_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/okta/terraform-provider-okta/okta/acctest"
+)
+
+func TestAccOauth2V1ClientsRoleHelpDeskAdmin_basic(t *testing.T) {
+	mgr := newFixtureManager("resources", "okta_oauth2_v1_clients_role_help_desk_admin", t.Name())
+	config := mgr.GetFixtures("basic.tf", t)
+	resourceName := fmt.Sprintf("%s.test", "okta_oauth2_v1_clients_role_help_desk_admin")
+	acctest.OktaResourceTest(t, resource.TestCase{
+		PreCheck:                 acctest.AccPreCheck(t),
+		ErrorCheck:               testAccErrorChecks(t),
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactoriesForTestAcc(t),
+		CheckDestroy:             nil,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "assignment_type"),
+					resource.TestCheckResourceAttrSet(resourceName, "created"),
+					resource.TestCheckResourceAttrSet(resourceName, "label"),
+					resource.TestCheckResourceAttrSet(resourceName, "last_updated"),
+					resource.TestCheckResourceAttrSet(resourceName, "status"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					r, ok := s.RootModule().Resources[resourceName]
+					if !ok {
+						return "", fmt.Errorf("Unable to find resource: %s", resourceName)
+					}
+					return fmt.Sprintf("%s/%s", r.Primary.Attributes["client_id"], r.Primary.Attributes["id"]), nil
+				},
+			},
+		},
+	})
+}
