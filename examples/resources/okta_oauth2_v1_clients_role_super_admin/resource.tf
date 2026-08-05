d8223d3bd90ad258f14d6b49b0b51aefb59f6c8d
Pranav RK <pranav.rathinakumar@okta.com>
2026-08-05 11:32:42 +0530

    Revert "feat: add new resources for oauthv1 clients (#2886)"
    This reverts commit 8afa5444d30b0ff7f684ab2eb501c1e83464a1c0.

--- examples/resources/okta_oauth2_v1_clients_role_super_admin/resource.tf
+++
@@ -1,4 +0,0 @@
-resource "okta_oauth2_v1_clients_role_super_admin" "example" {
-  client_id = "<client_id>"
-  type      = "SUPER_ADMIN"
-}
--- examples/resources/okta_oauth2_v1_clients_role_workflows_admin/resource.tf
+++
@@ -1,4 +0,0 @@
-resource "okta_oauth2_v1_clients_role_workflows_admin" "example" {
-  client_id = "<client_id>"
-  type      = "WORKFLOWS_ADMIN"
-}
