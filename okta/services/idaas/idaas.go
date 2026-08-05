7d8342e86dad3372625193e39b547b0db60a3337
Pranav RK <pranav.rathinakumar@okta.com>
2026-08-05 12:13:25 +0530

    Revert "feat: update okta-org-captcha resource (#2885)" (#2914)
    This reverts commit 71339765df3e40bc27258fc063814cd8afc72c4d.

--- okta/services/idaas/idaas.go
+++ okta/services/idaas/idaas.go
@@ -134,8 +134,6 @@ func FWProviderResources() []func() resource.Resource {
 		newIdentitySourceGroupMembershipResource,
 		newIdentitySourceImportResource,
 		newIdentitySourceUserResource,
-		newCaptchaResource,
-		newOrgCaptchaResource,
 		newOauth2V1ClientsRoleAccessCertificationsAdminResource,
 		newOauth2V1ClientsRoleAccessRequestsAdminResource,
 		newOauth2V1ClientsRoleApiAccessManagementAdminResource,
@@ -200,8 +198,6 @@ func FWProviderDataSources() []func() datasource.DataSource {
 		newAuthorizationServersPoliciesRuleDataSource,
 		newIamAssigneesUserDataSource,
 		newIamResourceSetDataSource,
-		newCaptchaDataSource,
-		newOrgCaptchaDataSource,
 	}
 }
 
@@ -239,6 +235,7 @@ func ProviderResources() map[string]*schema.Resource {
 		resources.OktaIDaaSAuthServerPolicyRule:          resourceAuthServerPolicyRule(),
 		resources.OktaIDaaSAuthServerScope:               resourceAuthServerScope(),
 		resources.OktaIDaaSBehavior:                      resourceBehavior(),
+		resources.OktaIDaaSCaptcha:                       resourceCaptcha(),
 		resources.OktaIDaaSCaptchaOrgWideSettings:        resourceCaptchaOrgWideSettings(),
 		resources.OktaIDaaSDomain:                        resourceDomain(),
 		resources.OktaIDaaSDomainCertificate:             resourceDomainCertificate(),
--- okta/services/idaas/resource_okta_captcha_test.go
+++ okta/services/idaas/resource_okta_captcha_test.go
@@ -1,52 +1,49 @@
-// Copyright 2025 - Present Okta, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package idaas_test
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/okta/terraform-provider-okta/okta/acctest"
+	"github.com/okta/terraform-provider-okta/okta/resources"
+	"github.com/okta/terraform-provider-okta/okta/utils"
 )
 
-func TestAccCaptcha_basic(t *testing.T) {
-	mgr := newFixtureManager("resources", "okta_captcha", t.Name())
+func TestAccResourceOktaCaptcha_crud(t *testing.T) {
+	mgr := newFixtureManager("resources", resources.OktaIDaaSCaptcha, t.Name())
 	config := mgr.GetFixtures("basic.tf", t)
-	resourceName := fmt.Sprintf("%s.test", "okta_captcha")
-	acctest.OktaResourceTest(t, resource.TestCase{
-		PreCheck:                 acctest.AccPreCheck(t),
-		ErrorCheck:               testAccErrorChecks(t),
-		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactoriesForTestAcc(t),
-		CheckDestroy:             nil,
-		Steps: []resource.TestStep{
-			{
-				Config: config,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(resourceName, "id"),
-					resource.TestCheckResourceAttrSet(resourceName, "name"),
-					resource.TestCheckResourceAttrSet(resourceName, "site_key"),
-					resource.TestCheckResourceAttrSet(resourceName, "type"),
-				),
+	updated := mgr.GetFixtures("updated.tf", t)
+	resourceName := fmt.Sprintf("%s.test", resources.OktaIDaaSCaptcha)
+	acctest.OktaResourceTest(
+		t, resource.TestCase{
+			PreCheck:                 acctest.AccPreCheck(t),
+			ErrorCheck:               testAccErrorChecks(t),
+			ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactoriesForTestAcc(t),
+			CheckDestroy:             checkResourceDestroy(resources.OktaIDaaSCaptcha, doesCaptchaExist),
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr(resourceName, "name", acctest.BuildResourceName(mgr.Seed)),
+						resource.TestCheckResourceAttr(resourceName, "type", "HCAPTCHA"),
+						resource.TestCheckResourceAttr(resourceName, "site_key", "random_key"),
+					),
+				},
+				{
+					Config: updated,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr(resourceName, "name", acctest.BuildResourceName(mgr.Seed)+"_updated"),
+						resource.TestCheckResourceAttr(resourceName, "type", "HCAPTCHA"),
+						resource.TestCheckResourceAttr(resourceName, "site_key", "random_key_updated")),
+				},
 			},
-			{
-				ResourceName:            resourceName,
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"secret_key"},
-			},
-		},
-	})
+		})
+}
+
+func doesCaptchaExist(id string) (bool, error) {
+	client := iDaaSAPIClientForTestUtil.OktaSDKSupplementClient()
+	_, response, err := client.GetCaptcha(context.Background(), id)
+	return utils.DoesResourceExist(response, err)
 }
